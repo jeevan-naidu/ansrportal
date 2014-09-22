86a79eaef61a405db917501abb0a473ee6dda667
Niranj Rajasekaran <niranj@fantain.com>
2014-09-22 19:16:37 +0530

    PEP8 Standards, User model not done in admin. Updated timezone.

--- timesheet/admin.py
+++ timesheet/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from timesheet.models import project, timeSheetEntry, ProjectChangeInfo, ProjectMilestone, ProjectTeamMember
 
 # Register your models here.
+
+class projectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'startDate', 'endDate', 'plannedEffort', 'projectManager')
+    search_fields = ('name', 'projectManager')
+
+class timeSheetEntryAdmin(admin.ModelAdmin):
+    list_display = ('project')
+    search_fields = ('project')
+
+class projectChangeInfoAdmin(admin.ModelAdmin):
+    list_display = ('firstName', 'emailAddress', 'ResidentialAddress', 'mobileNumber')
+    search_fields = ('firstName', 'mobileNumber')
+
+class projectMileStoneAdmin(admin.ModelAdmin):
+    list_display = ('docName', 'specialization', 'experience')
+    search_fields = ('specialization','docName')
+
+class projectTeamMemberAdmin(admin.ModelAdmin):
+    list_display = ('firstName', 'emailAddress', 'ResidentialAddress', 'mobileNumber')
+    search_fields = ('firstName', 'mobileNumber')
+
+admin.site.register(project)
+admin.site.register(timeSheetEntry)
+admin.site.register(ProjectChangeInfo)
+admin.site.register(ProjectMilestone)
+admin.site.register(ProjectTeamMember)
--- timesheet/models.py
+++ timesheet/models.py
@@ -1,35 +1,83 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+# ansr models
 
-class Project(models.Model):
-    name = models.CharField(max_length=50)
+# Follow pep8 standards for python class and field names
 
-class TSEntry(models.Model):
-    project = models.ForeignKey(Project)
-    wkstart = models.DateField(verbose_name = 'Week Start', auto_now_add = True)
-    wkend = models.DateField(verbose_name = 'Week Ending', auto_now_add = True)
-    monday = models.IntegerField(default=0)
-    approved = models.BooleanField(default=False)
+class project(models.Model):
+    name = models.CharField(max_length=50, verbose_name = "Project Name", default=None, blank=True)
+    startDate = models.DateTimeField(verbose_name = "Project Start Date", auto_now_add = True, default=None)
+    endDate = models.DateTimeField(verbose_name = "Project End Date", auto_now_add = True, default=None)
+    plannedEffort = models.IntegerField(default=0, verbose_name = "Planned Effort")
+    contigencyEffort = models.IntegerField(default=0, verbose_name = "Contigency")
+    projectManager = models.ForeignKey(User, verbose_name = "Project Manager", default=None)
+    # Record Entered / Updated Date
+    createdOn = models.DateTimeField(verbose_name = "created Date", auto_now_add = True, default=None)
+    updatedOn = models.DateTimeField(verbose_name = "Updated Date", auto_now_add = True, default=None)
+
+    def __unicode__(self):
+        return self.name
+
+class timeSheetEntry(models.Model):
+    project = models.ForeignKey(project, verbose_name = "Project Name")
+    # Week details
+    wkstart = models.DateField(default=None, blank=True, verbose_name = "Week Start")
+    wkend = models.DateField(default=None, blank=True, verbose_name = "Week End")
+    # Effort capture
+    monday = models.IntegerField(default=0, verbose_name = "M")
+    tuesday = models.IntegerField(default=0, verbose_name = "T")
+    wednesday = models.IntegerField(default=0, verbose_name = "W")
+    thursday = models.IntegerField(default=0, verbose_name = "T")
+    friday = models.IntegerField(default=0, verbose_name = "F")
+    saturday = models.IntegerField(default=0, verbose_name = "S")
+    sunday = models.IntegerField(default=0, verbose_name = "S")
+    approved = models.BooleanField(default=False, verbose_name = "Approved")
+    # Approval related details
+    approvedon = models.DateTimeField(default=None, blank=True, verbose_name = "Approved On")
+    managerFeedback = models.CharField(default=None, blank=True, max_length=1000, verbose_name = "Manager Feedback")
+    teamMember = models.ForeignKey(User, verbose_name = "Team Members")
+    questionsCreated = models.IntegerField(default=0, verbose_name = "Question Created")
+    # Record Entered / Updated Date
+    createdOn = models.DateTimeField(verbose_name = "created Date", auto_now_add = True, default=None)
+    updatedOn = models.DateTimeField(verbose_name = "Updated Date", auto_now_add = True, default=None)
+
+    class Meta:
+        verbose_name = 'Timesheet Entry'
+        verbose_name_plural = 'Timesheet Entries'
 
 
 class ProjectChangeInfo(models.Model):
-    project = models.ForeignKey(Project)
-    changed_on = models.DateField(auto_now_add = True)
-    cr_id = models.CharField(default=None, blank = True, max_length=100)
-    reason = models.CharField(max_length=100)
-    end_date = models.DateField(verbose_name = 'Revised Project End Date', auto_now_add = True, null=True, blank=True)
-    revised_effort = models.IntegerField(default=0)
-    approved = models.BooleanField(default=False)
+    # project change Request Fields
+    project = models.ForeignKey(project, verbose_name = "Project Name")
+    changedOn = models.DateField(auto_now_add = True, verbose_name = "Changed On", default=None)
+    crId = models.CharField(default=None, blank = True, max_length=100, verbose_name = "Change Request ID")
+    reason = models.CharField(max_length=100, default=None, blank=True, verbose_name = "Reason for change")
+    endDate = models.DateField(verbose_name = 'Revised Project End Date', auto_now_add = True, default=None)
+    revisedEffort = models.IntegerField(default=0, verbose_name = "Revised Effor")
+    closed = models.BooleanField(default=False, verbose_name = "Close Project")
+    # Check lateset change or not
+    status = models.BooleanField(default=False, verbose_name = "Status")
+    # Record Entered / Updated Date
+    createdOn = models.DateTimeField(verbose_name = "created Date", auto_now_add = True, default=None)
+    updatedOn = models.DateTimeField(verbose_name = "Updated Date", auto_now_add = True, default=None)
 
 
-class ProjectMilestones(models.Model):
-    project = models.ForeignKey(Project)
-    milestone_date = models.DateField(verbose_name = 'Milestone Date', default=None, auto_now_add = True)
-    description = models.CharField(default=None, blank=True, max_length=100)
+
+class ProjectMilestone(models.Model):
+    project = models.ForeignKey(project, verbose_name = "Project Name")
+    milestoneDate = models.DateField(verbose_name = "Milestone Date", default=None, auto_now_add = True)
+    deliverables = models.CharField(default=None, blank=True, max_length=100, verbose_name = "Deliverables")
+    description = models.CharField(default=None, blank=True, max_length=1000, verbose_name = "Description")
+    # Record Entered / Updated Date
+    createdOn = models.DateTimeField(verbose_name = "created Date", auto_now_add = True, default=None)
+    updatedOn = models.DateTimeField(verbose_name = "Updated Date", auto_now_add = True, default=None)
 
 
-class ProjectTeam(models.Model):
-    project = models.ForeignKey(Project)
-    member = models.ForeignKey(User)
+class ProjectTeamMember(models.Model):
+    project = models.ForeignKey(project,verbose_name = "Project Name")
+    member = models.ForeignKey(User, verbose_name = "Team Member")
+    plannedEffort = models.IntegerField(default=0, verbose_name = "Planned Effort")
+    # Record Entered / Updated Date
+    createdOn = models.DateTimeField(verbose_name = "created Date", auto_now_add = True, default=None)
+    updatedOn = models.DateTimeField(verbose_name = "Updated Date", auto_now_add = True, default=None)
