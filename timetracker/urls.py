89436cfdd1085609b57107f3fcc311864c007be9
Yuvaraj Ramesh <yuvaraj@yuvaraj-Inspiron-3542.(none)>
2014-11-10 13:43:08 +0530

    test1

--- timetracker/urls.py
+++ timetracker/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 from timesheet import apps
+from employee.views import logincred
 
 admin.autodiscover()
 
@@ -13,5 +14,6 @@ urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^timesheet/', include('timesheet.urls')),
                        url(r'^chaining/', include('smart_selects.urls')),
+		       url(r'^login$', 'employee.views.logincred', name='logincred'),
                        url(r'^$', RedirectView.as_view(url='/timesheet/')),
                        )
