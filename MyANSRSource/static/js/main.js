9fdab98a87c43ed5629a88df545c9d41895e8d9a
surenderLohia <surender@fantain.com>
2014-12-12 16:07:12 +0530

    1. WIP - 1.

--- MyANSRSource/static/js/main.js
+++ MyANSRSource/static/js/main.js
@@ -214,18 +214,6 @@ app.calcCurRowChangeDate = function() {
             });
         }
 
-
-
-        var contigencyEffortEle = $('.contigency-effort-input');
-
-        if(contigencyEffortEle.length > 0) {
-            localStorage.contigencyEffort = contigencyEffortEle.val();
-        }
-
-        contigencyEffortEle.on('keyup', function() {
-            localStorage.contigencyEffort = $(this).val();
-        });
-
     });
 }());
 
@@ -807,6 +795,62 @@ app.getPlannedEffort = function($startDate, $endDate, $plannedEffort, $plannedPe
         plannedEffort: plannedEffort
     };
 };
+
+
+
+
+var confirmExitIfModified = (function() {
+    function formIsDirty(form) {
+        for (var i = 0; i < form.elements.length; i++) {
+            var element = form.elements[i];
+            var type = element.type;
+            if (type == "checkbox" || type == "radio") {
+                if (element.checked != element.defaultChecked) {
+                    return true;
+                }
+            }
+            else if (type == "hidden" || type == "password" ||
+                type == "text" || type == "textarea") {
+                if (element.value != element.defaultValue) {
+                    return true;
+                }
+            }
+            else if (type == "select-one" || type == "select-multiple") {
+                for (var j = 0; j < element.options.length; j++) {
+                    if (element.options[j].selected !=
+                        element.options[j].defaultSelected) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    return function(form, message) {
+        window.onbeforeunload = function(e) {
+            e = e || window.event;
+            if (formIsDirty(document.forms[form])) {
+                // For IE and Firefox
+                if (e) {
+                    e.returnValue = message;
+                }
+                // For Safari
+                return message;
+            }
+        };
+    };
+})();
+
+confirmExitIfModified('timesheetForm', "You have unsaved changes.");
+
+console.log('suren');
+
+
+
+
+
+
 
 
 
