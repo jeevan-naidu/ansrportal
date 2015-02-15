971bb3ece28caf41f0daa429b5bae9b5e97d3445
Vivek Venugopalan <vivek@fantain.com>
2015-02-15 21:19:03 +0530

    Migration error fixed

---
+++ MyANSRSource/migrations/0021_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('MyANSRSource', '0020_auto_20150214_0701'),
+        ('MyANSRSource', '0019_auto_20150212_1510'),
+    ]
+
+    operations = [
+    ]
