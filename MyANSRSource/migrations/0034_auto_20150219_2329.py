c7bc3a062a3a77819cd56d5cbc3c74ff4900aa64
Niranj Rajasekaran <niranj@fantain.com>
2015-02-23 09:35:59 +0530

    WIP

--- MyANSRSource/migrations/0034_auto_20150219_2329.py
+++ MyANSRSource/migrations/0034_auto_20150219_2329.py
@@ -9,26 +9,34 @@ def populateValues(apps, schema_editor):
     activityModel = apps.get_model("MyANSRSource", "Activity")
     taskModel = apps.get_model("MyANSRSource", "Task")
 
-    ts = tsModel.objects.all().values('task', 'activity')
-    for eachData in ts:
+    ts = tsModel.objects.filter(project__isnull=False).values('activity',
+                                                              'task',
+                                                              'project__projectType')
+    for eachTs in ts:
         try:
-            act = activityModel.objects.create(code=eachData['activity'])
-            ts.activity1 = act
+            act = activityModel.objects.create(code=eachTs['activity'],
+                                               name=eachTs['activity'])
+            eachTs.activity1 = act
             act.save()
         except IntegrityError:
             pass
         try:
-            tsk = taskModel.objects.create(code=eachData['task'])
-            ts.task1 = tsk
+            tsk = taskModel.objects.create(code=eachTs['task'],
+                                           name=eachTs['task'],
+                                           projectType.id=eachTs['project__projectType'])
+            eachTs.task1 = tsk
             tsk.save()
         except IntegrityError:
             pass
+        eachTs.save()
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('MyANSRSource', '0042_auto_20150222_1937'),
+        ('MyANSRSource', '0026_auto_20150217_1621'),
+        ('MyANSRSource', '0027_auto_20150217_1629'),
     ]
 
     operations = [
--- MyANSRSource/migrations/0036_auto_20150219_2344.py
+++ MyANSRSource/migrations/0036_auto_20150219_2344.py
@@ -5,17 +5,8 @@ from django.db import models, migrations
 
 
 def populateValues(apps, schema_editor):
-    tsModel = apps.get_model("MyANSRSource", "TimeSheetEntry")
-    activityModel = apps.get_model("MyANSRSource", "Activity")
-    taskModel = apps.get_model("MyANSRSource", "Task")
+    pass
 
-    ts = tsModel.objects.all().values('id', 'task', 'activity')
-    for eachData in ts:
-        act = activityModel.objects.get(code=eachData['activity'])
-        tsk = taskModel.objects.get(code=eachData['task'])
-        tsObj = tsModel.objects.get(pk=ts.id)
-        tsObj.update(activity=act, task=tsk)
-        tsObj.save()
 
 
 class Migration(migrations.Migration):
