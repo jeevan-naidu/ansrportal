3c5376f34a89cefb60965489ecc018ee3171e10c
Niranj Rajasekaran <niranj@fantain.com>
2015-02-24 18:12:19 +0530

    Changed member filter to email

--- MyANSRSource/autocomplete_light_registry.py
+++ MyANSRSource/autocomplete_light_registry.py
@@ -11,7 +11,8 @@ class AutocompleteUser(autocomplete_light.AutocompleteModelBase):
         choices = self.choices.filter(
             Q(is_superuser=False)
         )
-        choices = choices.filter(username__icontains=q)
+
+        choices = choices.filter(email__icontains=q)
         return self.order_choices(choices)[0:self.limit_choices]
 
 autocomplete_light.register(User, AutocompleteUser)
