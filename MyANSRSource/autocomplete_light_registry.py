a109db0cd7b66d863ca2473c4ebf211daa541e3b
Niranj Rajasekaran <niranj@fantain.com>
2015-02-24 23:06:57 +0530

    Changed Book to smart select feature

--- MyANSRSource/autocomplete_light_registry.py
+++ MyANSRSource/autocomplete_light_registry.py
@@ -1,6 +1,7 @@
 import autocomplete_light
 from django.contrib.auth.models import User
 from django.db.models import Q
+from MyANSRSource.models import Book
 
 
 class AutocompleteUser(autocomplete_light.AutocompleteModelBase):
@@ -16,3 +17,14 @@ class AutocompleteUser(autocomplete_light.AutocompleteModelBase):
         return self.order_choices(choices)[0:self.limit_choices]
 
 autocomplete_light.register(User, AutocompleteUser)
+
+
+class AutocompleteBook(autocomplete_light.AutocompleteModelBase):
+    autocomplete_js_attributes = {'placeholder': 'Enter a member name'}
+
+    def choices_for_request(self):
+        q = self.request.GET.get('q', '')
+        choices = self.choices.filter(name__icontains=q)
+        return self.order_choices(choices)[0:self.limit_choices]
+
+autocomplete_light.register(Book, AutocompleteBook)
