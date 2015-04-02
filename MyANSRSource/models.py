df1c02067a80243db1db904aa315493b31674c7f
Niranj Rajasekaran <niranj@fantain.com>
2015-04-02 12:06:23 +0530

    Added ISBN for book

--- MyANSRSource/models.py
+++ MyANSRSource/models.py
@@ -19,6 +19,8 @@ class Book(models.Model):
     author = models.CharField(max_length=100, null=False,
                               verbose_name="Author")
     edition = models.CharField(max_length=30, null=True, blank=True)
+    isbn = models.IntegerField(default=0,
+                               verbose_name="ISBN")
     createdOn = models.DateTimeField(verbose_name="created Date",
                                      auto_now_add=True)
     updatedOn = models.DateTimeField(verbose_name="Updated Date",
