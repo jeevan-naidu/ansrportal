765eeb22905bc908dd9d31aa7c4fc2473d7cd0a4
surenderLohia <surender@fantain.com>
2015-04-13 13:12:05 +0530

    Some updates - WIP...

--- MyANSRSource/static/js/main.js
+++ MyANSRSource/static/js/main.js
@@ -42,9 +42,9 @@ helper.getValOrText = function($ele) {
     return eleVal;
 };
 
-app.billableSetZeroList = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
-                           16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 
-                           28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 
+app.billableSetZeroList = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                           16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+                           28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
                            40, 41, 42, 43, 44, 45, 46, 47, 50, 51, 52,
                            53, 54, 55];
 
@@ -738,7 +738,13 @@ app.getSum = function($elements, $outputElement) {
                 $element,
                 curId,
                 curName,
-                $curIdSel;
+                $curIdSel,
+                $popover = $('.popover');
+
+            if($popover.length > 0) {
+                $popover.hide();
+            }
+
 
         // Slice the id number from last row id
         lastRowId = app.getIdNo(lastRowId);
