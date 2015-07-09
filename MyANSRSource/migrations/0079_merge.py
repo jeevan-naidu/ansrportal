a7b938e551902ddb4358ae768a24657a68922fa8
Vivek Venugopalan <vivek@fantain.com>
2015-07-09 12:32:36 +0530

    Added missing migration

---
+++ MyANSRSource/migrations/0079_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('MyANSRSource', '0076_merge'),
+        ('MyANSRSource', '0078_merge'),
+    ]
+
+    operations = [
+    ]
