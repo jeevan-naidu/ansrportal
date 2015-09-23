4f7f3a70c68f7e25085f6cd240891e2ff432e096
Niranj Rajasekaran <niranj@fantain.com>
2015-09-23 17:24:19 +0530

    Added comments system in approve ts

--- MyANSRSource/views.py
+++ MyANSRSource/views.py
@@ -809,6 +809,7 @@ def ApproveTimesheet(request):
             mem = "mem" + str(i)
             start = "start" + str(i)
             end = "end" + str(i)
+            fb = "fb" + str(i)
             if start in request.POST:
                 startDate = date.fromordinal(int(request.POST.get(start)))
             if end in request.POST:
@@ -824,9 +825,11 @@ def ApproveTimesheet(request):
                         if request.POST.get(choice) == 'redo':
                             eachTS.hold = False
                             eachTS.approved = False
+                            eachTS.managerFeedback = request.POST.get(fb)
                         else:
                             eachTS.hold = True
                             eachTS.approved = True
+                            eachTS.managerFeedback = request.POST.get(fb)
                         eachTS.save()
         return HttpResponseRedirect('/myansrsource/dashboard')
     else:
