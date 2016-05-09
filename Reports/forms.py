024128b2c2f682c413d733132074aa73710eacb0
amol <amol.pachpute@ansrsource.com>
2016-05-09 12:30:42 +0530

    Milestone reports

--- Reports/forms.py
+++ Reports/forms.py
@@ -2,48 +2,42 @@ from django import forms
 from MyANSRSource.models import Project
 import autocomplete_light
 from bootstrap3_datetime.widgets import DateTimePicker
+
 autocomplete_light.autodiscover()
 
-dateTimeOption = {"format": "DD/MM/YYYY", "pickTime": False}
-MILESTONE_CHOICES= (('financial', 'Financial'), ('non_financial','Non-Financial'))
-MILESTONE_STATUS_CHOICES= (('completed', 'Completed'), ('not_completed','Not Completed'))
+dateTimeOption = {"format": "MM/DD/YYYY", "pickTime": False}
+MILESTONE_CHOICES = (('any', 'Any'), ('financial', 'Financial'), ('non_financial', 'Non-Financial'))
+MILESTONE_STATUS_CHOICES = (('any', 'Any'), ('completed', 'Completed'), ('not_completed', 'Not Completed'))
+
 
 class MilestoneReportsForm(forms.Form):
-    
-    project = forms.ModelChoiceField(queryset=Project.objects.all(),
-                                          widget=autocomplete_light.ChoiceWidget('ProjectAutocompleteProjects'))
-    
+    project = forms.ModelChoiceField(help_text="Leave blank for all", queryset=Project.objects.all(),
+                                     widget=autocomplete_light.ChoiceWidget('ProjectAutocompleteProjects'),
+                                     required=False)
+
     project.widget.attrs = {'class': 'form-control', 'placeholder': 'Enter Project Id/Name'}
-    
+
     from_date = forms.DateField(
         label="From",
         widget=DateTimePicker(options=dateTimeOption),
     )
-    from_date.widget.attrs = {'class': 'form-control filter_class'}
-    
+    from_date.widget.attrs = {'class': 'form-control filter_class', 'required': 'true'}
+
     to_date = forms.DateField(
         label="To",
         widget=DateTimePicker(options=dateTimeOption),
     )
-    to_date.widget.attrs = {'class': 'form-control filter_class'}
-    
+    to_date.widget.attrs = {'class': 'form-control filter_class', 'required': 'true'}
+
     milestone_type = forms.ChoiceField(choices=MILESTONE_CHOICES)
     milestone_type.widget.attrs = {'class': 'form-control'}
-    
+
     milestone_status = forms.ChoiceField(choices=MILESTONE_STATUS_CHOICES)
     milestone_status.widget.attrs = {'class': 'form-control'}
-    
-    #project = forms.ModelChoiceField(
-    #    queryset=None,
-    #    label="Project",
-    #    required=True,
-    #)
-    #
-    #def __init__(self, *args, **kwargs):
-    #    super(MilestoneReportsForm, self).__init__(*args, **kwargs)
-    #    self.fields['project'].queryset = Project.objects.all()
-    #    self.fields['project'].widget = autocomplete_light.ChoiceWidget('ProjectAutocompleteProjects')
-    #    self.fields['project'].widget.attrs['class'] = "form-control"
-    #    self.fields['project'].widget.attrs['placeholder'] = 'Enter a Project Name /Project Id'
 
- 
+    def clean(self):
+        # cleaned_data = super(MilestoneReportsForm, self).clean()
+        from_date = self.cleaned_data.get('from_date')
+        to_date = self.cleaned_data.get('to_date')
+        if from_date > to_date:
+            self.add_error('from_date', "From date should be less than to date")
--- Reports/views.py
+++ Reports/views.py
@@ -1,18 +1,121 @@
 from django.shortcuts import render
 from django.views.generic import View
 from Reports.forms import MilestoneReportsForm
-# Create your views here.
+from MyANSRSource.models import ProjectMilestone
+from django.forms.fields import DateField
+import csv
+from django.http import HttpResponse
+import datetime
+date_instance = DateField()
+
 
 class MilestoneReportsView(View):
     ''' MilestoneReportsView '''
-    
-    
+
     def get(self, request):
         
         context_data = {'form':None}
         form = MilestoneReportsForm()
         context_data['form'] = form
         return render(request, 'milestone_reports.html', context_data)
-    
+
     def post(self, request):
-        pass
+
+        form = MilestoneReportsForm(request.POST)
+        context_data = {'form': form, 'errors': ""}
+
+        if form.is_valid():
+
+            from_date = request.POST['from_date'].replace("/", "")
+            to_date = request.POST['to_date'].replace("/", "")
+            from_date = datetime.datetime.strptime(from_date, "%m%d%Y").date()
+            to_date = datetime.datetime.strptime(to_date, "%m%d%Y").date()
+
+            if request.POST['milestone_type'] == 'financial':
+                financial = True
+            elif request.POST['milestone_type'] == 'non_financial':
+                financial = False
+            else:
+                financial = " "
+
+            if request.POST['milestone_status'] == 'completed':
+                closed = True
+            elif request.POST['milestone_status'] == 'not_completed':
+                closed = False
+            else:
+                closed = " "
+
+            if request.POST.get('project', '') and closed == " " and financial == " ":
+                # Project - Yes, closed - Any, financial - any
+                MilestoneList = ProjectMilestone.objects.filter(project=request.POST['project'],
+                                                                milestoneDate__gte = from_date,
+                                                                milestoneDate__lte = to_date,
+                                                                )
+            elif request.POST.get('project', '') and closed != " " and financial == " ":
+                # Project - Yes, closed - either True or False, financial - any
+                MilestoneList = ProjectMilestone.objects.filter(project=request.POST['project'],
+                                                                milestoneDate__gte=from_date,
+                                                                milestoneDate__lte=to_date,
+                                                                closed=closed
+                                                                )
+
+            elif request.POST.get('project', '') and financial != " " and closed == " ":
+                # Project - Yes, closed - any, financial - either True or False
+                MilestoneList = ProjectMilestone.objects.filter(project=request.POST['project'],
+                                                                milestoneDate__gte=from_date,
+                                                                milestoneDate__lte=to_date,
+                                                                financial=financial
+                                                                )
+            elif request.POST.get('project', '') and financial != " " and closed != " ":
+                # Project - Yes, closed - any, financial - either True or False
+                MilestoneList = ProjectMilestone.objects.filter(project=request.POST['project'],
+                                                                milestoneDate__gte=from_date,
+                                                                milestoneDate__lte=to_date,
+                                                                financial=financial,
+                                                                closed=closed
+                                                                )
+
+            elif not request.POST.get('project', '') and closed != " " and financial != " ":
+                # Project - Yes, closed - either True or False, financial - any
+                MilestoneList = ProjectMilestone.objects.filter(milestoneDate__gte=from_date,
+                                                                milestoneDate__lte=to_date,
+                                                                financial=financial,
+                                                                closed=closed
+                                                                )
+
+            elif not request.POST.get('project', '') and closed != " " and financial == " ":
+                # Project - any, closed - Teither True or False, financial - any
+                MilestoneList = ProjectMilestone.objects.filter(milestoneDate__gte=from_date,
+                                                                milestoneDate__lte=to_date,
+                                                                closed=closed
+                                                                )
+
+            elif not request.POST.get('project', '') and financial != " " and closed == " ":
+                # Project - any, financial - either True or False, closed - any
+                MilestoneList = ProjectMilestone.objects.filter(milestoneDate__gte=from_date,
+                                                                milestoneDate__lte=to_date,
+                                                                financial=financial,
+                                                                )
+            elif not request.POST.get('project', '') and closed == " " and financial == " ":
+                # Project - any, financial - any, closed - any
+                MilestoneList = ProjectMilestone.objects.filter(milestoneDate__gte=from_date,
+                                                                milestoneDate__lte=to_date,
+                                                                )
+
+            response = HttpResponse(content_type='text/csv')
+            filename = "MilestoneReports_" + request.POST['from_date'] + "_to_" + request.POST['to_date'] + ".csv"
+            response['Content-Disposition'] = 'attachment; filename="' + filename + '"'
+
+            writer = csv.writer(response)
+            writer.writerow(['Project Id', 'Project Name', 'Milestone Date', 'Description', 'Amount', 'Closed Status',
+                             'Is Financial', 'Closed On', 'Updated On'])
+            for obj in MilestoneList:
+                writer.writerow([obj.project.projectId, obj.project.name, obj.milestoneDate, obj.description,
+                                 obj.amount, obj.closed, obj.financial, obj.closedon, obj.updatedOn])
+            if MilestoneList:
+                return response
+            else:
+                context_data['errors'] = "No Milestones found"
+        return render(request, 'milestone_reports.html', context_data)
+
+
