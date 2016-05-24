45d21b1bb2118d34550c4200a1dfa1f92afaa896
admin1 <admin1>
2016-05-24 10:46:14 +0530

    added more links

--- Leave/urls.py
+++ Leave/urls.py
@@ -7,5 +7,10 @@ urlpatterns = patterns(u'',
                        url(r'^add/$', login_required(ApplyLeaveView.as_view()), name=u'leave_list'),
                        url(r'^cancel/$', login_required(LeaveCancel), name=u'leave_cancel'),
                        url(r'^details/$', login_required(LeaveDetails), name=u'leave_details'),
-                       url(r'^transaction/$', login_required(LeaveTransaction), name=u'LeaveTransaction' )
+                       url(r'^transaction/$', login_required(LeaveTransaction), name=u'LeaveTransaction'),
+                       url(r'^leavelist/$', login_required(LeaveListView.as_view()), name=u'list_leave'),
+                       url(r'^leavelist/(?P<all>[a-z]+)/$', login_required(LeaveListView.as_view()),
+                           name=u'list_leave_all'),
+                       url(r'^manage/$', login_required(LeaveManageView.as_view(template_name='Manager.html')),
+                           name=u'manage_leave_list'),
                        )
