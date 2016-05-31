714439bfd2aa1a93b0f3f0f370faf2ca9475c38e
amol <amol.pachpute@ansrsource.com>
2016-05-31 19:21:38 +0530

    salesforce - help text change

--- Salesforce/forms.py
+++ Salesforce/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class UploadSalesforceDataForm(forms.Form):
-    salesforce_data_file = forms.FileField(required=True, help_text="Please use semicolon delimited csv")
+    salesforce_data_file = forms.FileField(required=True, help_text="Please use semicolon delimited csv only")
     salesforce_data_file.widget.attrs = {'class':'filestyle', 'data-buttonBefore':'true', 'data-iconName':'glyphicon glyphicon-paperclip'}
