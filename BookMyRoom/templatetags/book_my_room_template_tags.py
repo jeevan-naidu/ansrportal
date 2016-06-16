b2eaf4edf3585de686a11511e18e2ecd16b535f3
amol <amol.pachpute@ansrsource.com>
2016-06-16 13:58:54 +0530

    backend development

---
+++ BookMyRoom/templatetags/book_my_room_template_tags.py
@@ -0,0 +1,44 @@
+from django import template
+from Grievances.models import SATISFACTION_CHOICES
+register = template.Library()
+from datetime import datetime
+#from django.utils.timezone import localtime
+#from django.utils.timezone import get_default_timezone
+from BookMyRoom.models import RoomDetail, MeetingRoomBooking
+import datetime
+from django.utils import timezone
+import pytz
+
+@register.filter
+def Concat(for_date, time_period):
+    """ return cancatination of 'for_date' and 'time_period' seperated by semicolon. This is provided to 'IsBooked' template tag function"""
+    
+    return str(for_date) +";"+ str(time_period)
+
+@register.filter
+def IsBooked(for_date_time_period, room_obj):
+    
+    temp_list = for_date_time_period.split(";")
+    for_date = temp_list[0]
+    time_period= temp_list[1]
+    for_date_obj = datetime.datetime.strptime(for_date + "," + time_period.split("-")[0],  "%Y-%m-%d,%H:%M")
+    utc_datetime_obj = timezone.make_aware(for_date_obj, timezone.get_current_timezone())
+    utc_datetime_obj = utc_datetime_obj.astimezone(pytz.utc)
+    obj_list = MeetingRoomBooking.objects.filter(from_time=utc_datetime_obj, room=room_obj, active=True)
+    
+    return True if obj_list else False
+
+
+
+#@register.filter
+#def IsNew(registered_date):
+#    current_date = datetime.now().replace(tzinfo=get_default_timezone())
+#    registered_date = localtime(registered_date)
+#    diff = current_date - registered_date
+#    if diff.days < 5:
+#        return True
+#    else:
+#        return False
+#
+
+#register.filter('GetFileNamefromPath', GetFileNamefromPath)
