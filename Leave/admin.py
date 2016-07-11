e3a7519b95b4c956fb6fce2344527e44002cc090
vivek.pradhan <vivek.pradhan@ansrsource.com>
2016-07-11 16:58:24 +0530

    code comment for migrations

--- Leave/admin.py
+++ Leave/admin.py
@@ -1,11 +1,11 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from models import LeaveType, LeaveSummary, LeaveApplications
-# Register your models here.
-
-
-admin.site.register(LeaveType)
-admin.site.register(LeaveSummary)
-admin.site.register(LeaveApplications)
+# from django.contrib import admin
+#
+# # Register your models here.
+# from django.contrib import admin
+# from models import LeaveType, LeaveSummary, LeaveApplications
+# # Register your models here.
+#
+#
+# admin.site.register(LeaveType)
+# admin.site.register(LeaveSummary)
+# admin.site.register(LeaveApplications)
