6d46465054df8ccc898755ad65cbcc32804439de
vivekpradhanansr <vivek.pradhan@ansrsource.com>
2016-09-21 11:20:55 +0530

    library module

---
+++ Library/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+from views import *
+from django.contrib.auth.decorators import login_required
+from Library import views
+urlpatterns = patterns(u'',
+                       url(r'^$', login_required(dashboard), name=u'Library_dashboard'),
+                       url(r'^bookrent/$', login_required(bookrent), name=u'Library_bookrent'),
+                       url(r'^adminaction/$', login_required(adminaction), name=u'Library_adminaction'),
+                       url(r'^bookreturn/$', login_required(bookreturn), name=u'Library_bookreturn'),
+                       )
