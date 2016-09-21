1ea632b9c88259b0fbc71684cf96a151e4c39d99
vivekpradhanansr <vivek.pradhan@ansrsource.com>
2016-09-21 18:24:07 +0530

    css changes

--- Library/urls.py
+++ Library/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns(u'',
                        url(r'^bookrent/$', login_required(bookrent), name=u'Library_bookrent'),
                        url(r'^adminaction/$', login_required(adminaction), name=u'Library_adminaction'),
                        url(r'^bookreturn/$', login_required(bookreturn), name=u'Library_bookreturn'),
+                       url(r'^booksearch/$', login_required(booksearch), name=u'Library_booksearch'),
                        )
