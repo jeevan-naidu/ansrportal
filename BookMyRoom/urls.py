859916de58c2ebed9a02c61ee2b849334fa105d4
balamurugan <balamurugan.rs@ansrsource.com>
2016-10-04 16:10:50 +0530

    1.9 changes

--- BookMyRoom/urls.py
+++ BookMyRoom/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from BookMyRoom.views import BookMeetingRoomView, GetBookingsView, CancelBooking, GetAllRoomsList, GetBookingDetails
 
-urlpatterns = patterns(u'',
+urlpatterns = [
                        url(r'^$', login_required(BookMeetingRoomView.as_view()), name=u''),
                        url(r'^details/$', login_required(GetBookingsView), name=u''),
                        url(r'^cancel/$', login_required(CancelBooking), name=u''),
@@ -10,4 +10,4 @@ urlpatterns = patterns(u'',
                        # Urls for android app
                        url(r'^get-bookings-details/$', GetBookingDetails, name=u''),
                        url(r'^get-all-rooms/$', GetAllRoomsList, name=u''),
-                       )
+                       ]
