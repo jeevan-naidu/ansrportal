34878f58b9d5bf879f1dc71573413d1c316168df
vivekpradhanansr <vivek.pradhan@ansrsource.com>
2016-11-09 18:27:23 +0530

    color trigger for employee

---
+++ employee/signals.py
@@ -0,0 +1,20 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from employee.models import Employee
+
+# method for updating
+@receiver(pre_save, sender=Employee)
+def update_color(sender, instance, **kwargs):
+    color_list = ['violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red']
+    colorindex = int(instance.user_id) % 7
+    color = color_list[colorindex]
+    instance.color = color
+
+
+
+
+
+
+
+
+
