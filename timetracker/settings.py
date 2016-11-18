e09dfefb8d4f416f8aa10ae5d79b95f49e8a6aae
vivekpradhanansr <vivek.pradhan@ansrsource.com>
2016-11-18 15:35:05 +0530

    setting changes for prod

--- timetracker/settings.py
+++ timetracker/settings.py
@@ -201,14 +201,15 @@ ROOT_URLCONF = 'timetracker.urls'
 WSGI_APPLICATION = 'timetracker.wsgi.application'
 
 
-
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "myansrsource",
         "USER": "root",
-        "PASSWORD": "root",
+        "PASSWORD": "mysqlroot",
         "HOST": "localhost",
         "PORT": "3306",
         },
@@ -222,7 +223,8 @@ BOOTSTRAP3 = {
 # Font awesome related settings
 FONTAWESOME_CSS_URL = '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'
 
-
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -327,7 +329,7 @@ FEED_DELIMITER = ","
 EXTERNAL_PROJECT_NOTIFIERS = ['sanjay.kunnath@ansrsource.com']
 
 # New Joinee Notifiers
-NEW_JOINEE_NOTIFIERS = ['shalini.bhagat@ansrsource.com']
+NEW_JOINEE_NOTIFIERS = ['hrhelpdesk@ansrsource.com']
 
 # Grappelli Customizations
 GRAPPELLI_ADMIN_TITLE = 'myansrsource administration'
@@ -345,16 +347,15 @@ BACKUPDIR = '/www/MyANSRSource/ansr-timesheet/backup'
 MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
 MEDIA_URL = '/media/'
 
-GRIEVANCES_ADMIN_EMAIL = "shalini.bhagat@ansrsource.com"
+GRIEVANCES_ADMIN_EMAIL = "HR4U@ansrsource.com"
 BOOKING_ROOM_ADMIN = "BookingRoomAdmin"
-LEAVE_ADMIN_EMAIL = ['shalini.bhagat@ansrsource.com']
+LEAVE_ADMIN_EMAIL = ['HR4U@ansrsource.com']
 
 MILESTONE_REPORTS_ADMIN_GROUP_NAME = "MilestoneReportsAdmin"
 
 SALESFORCE_ADMIN_GROUP_NAME = "SalesforceAdmin"
 
 LEAVE_ADMIN_GROUP = 'LeaveAdmin'
-
 LEAVE_SHORT_ATTENDANCE_ISACTIVE = True
 
 HIRE_RECRUITER = 'HireRecruiter'
