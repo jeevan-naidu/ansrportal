36168c4a50d9ba3d21c1df4775c4b6793f9795e1
pupul <pupul.ranjan@ansrsource.com>
2017-01-04 19:12:01 +0530

    date prepoulate

--- ExitApp/tasks.py
+++ ExitApp/tasks.py
@@ -32,9 +32,30 @@ class ExitEmailSendTask(Task):
             logger.debug('send successful')
 
 
-class PostAcceptedMail(Task):
+class PostAcceptedMailMGR(Task):
     def run(self, username, user_email, laste_date_accepted, ):
         msg_html = render_to_string('email_templates/acceptence_email.html',
+                                    {'registered_by': username,
+                                    'last_date_accepted': laste_date_accepted, })
+
+        mail_obj = EmailMessage('Resignation Acceptance',
+                                msg_html, settings.EMAIL_HOST_USER, ['pupul.ranjan@ansrsource.com', user_email],
+                                cc=['balamurugan.rs@ansrsource.com'])
+
+        mail_obj.content_subtype = 'html'
+        email_status = mail_obj.send()
+        if email_status == 0:
+                    logger.error(
+                        "Unable To send Mail To The Authorities For"
+                        "The Following Exit Applicant : Date time : ")
+                    return "failed"
+        else:
+            logger.debug('send successful')
+
+
+class PostAcceptedMailHR(Task):
+    def run(self, username, user_email, laste_date_accepted, ):
+        msg_html = render_to_string('email_templates/acceptence_emailhr.html',
                                     {'registered_by': username,
                                     'last_date_accepted': laste_date_accepted, })
 
@@ -113,8 +134,9 @@ class AdminClearanceMail(Task):
             logger.debug('send successful')
 
 
-# tasks.register(ExitEmailSendTask)
-# tasks.register(PostAcceptedMail)
+tasks.register(ExitEmailSendTask)
+tasks.register(PostAcceptedMailMGR)
+tasks.register(PostAcceptedMailHR)
 # tasks.register(AdminClearanceMail)
 # tasks.register(ITClearanceMail)
 # tasks.register(LibraryClearanceMail)
