68988c005bbebcda7f4a10b5d1c4ff09c54e152b
SanjayKunnath <Sanjay.Kunnath@ansrsource.com>
2017-02-03 17:39:14 +0530

    updated monthlyleave addition cron for credit table

--- Leave/management/commands/monthlyleaveaddition.py
+++ Leave/management/commands/monthlyleaveaddition.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from Leave.models import *
-from employee.models import Employee
-from datetime import date, timedelta
+from datetime import date
 from django.core.management.base import BaseCommand
 import logging
 
@@ -16,12 +15,21 @@ class Command(BaseCommand):
 
 def monthlyLeaveAdditionCron():
     users = User.objects.filter(is_active = True)
-
+    current_year = date.today().year
+    current_month = date.today().month
+    admin = User.objects.get(id=35)
     for user in users:
         try:
-            leaves = LeaveSummary.objects.filter(user = user, leave_type__occurrence = 'monthly',year=date.today().year)
+            leaves = LeaveSummary.objects.filter(user = user, leave_type__occurrence = 'monthly',year=current_year)
             for leave in leaves:
                 if leave.leave_type.carry_forward != 'none':
+                    CreditEntry.objects.create(user=user,
+                                               year=current_year,
+                                               month=current_month,
+                                               leave_type=leave.leave_type,
+                                               days=leave.leave_type.count,
+                                               status_action_by=admin,
+                                               comments="monthly leave credit")
                     leave.balance = float(leave.balance) + float((leave.leave_type.count).encode('utf-8'))
                     leave.save()
                     print "leave added for {0} {1} leave type id {2} balance is {3}".format(user.first_name,
@@ -29,6 +37,13 @@ def monthlyLeaveAdditionCron():
                                                                                             leave.leave_type_id,
                                                                                             leave.balance)
                 else:
+                    CreditEntry.objects.create(user=user,
+                                               year=current_year,
+                                               month=current_month,
+                                               leave_type=leave.leave_type,
+                                               days=leave.leave_type.count,
+                                               status_action_by=admin,
+                                               comments="monthly leave credit")
                     leave.balance = float((leave.leave_type.count).encode('utf-8'))
                     leave.save()
                     print "leave added for {0} {1} leave type id {2} balance is {3}".format(user.first_name,
