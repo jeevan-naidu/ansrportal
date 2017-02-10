b2c1040db85126f1473bbfb8cdd0543ffc399b84
Hakaishin <hakaishin07.com>
2017-02-10 14:57:34 +0530

    changing SIGUSR1 to SIGTERM

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import traceback
 
 if __name__ == "__main__":
     # print stack trace on USR1
-    signal.signal(signal.SIGUSR1, lambda sig, stack: traceback.print_stack(stack))
+    signal.signal(signal.SIGTERM, lambda sig, stack: traceback.print_stack(stack))
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timetracker.settings")
 
     from django.core.management import execute_from_command_line
