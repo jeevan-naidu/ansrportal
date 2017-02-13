b0b2074a352c0b51242ce89192fe962398bc2a49
balamurugan.rs <balamurugan.rs@ansrsource.com>
2017-02-13 15:12:33 +0530

    testing

--- MyANSRSource/autocomplete_light_registry.py
+++ MyANSRSource/autocomplete_light_registry.py
@@ -2,18 +2,23 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 from MyANSRSource.models import Book, Project
 from dal import autocomplete
-
+import logging
+logger = logging.getLogger('MyANSRSource')
 
 class AutocompleteUser(autocomplete.Select2QuerySetView):
 
     def get_queryset(self):
         q = self.request.GET.get('q', '')
-        print "q is" , q
-        choices = User.objects.filter(
-            Q(is_superuser=False) & Q(is_active=True)
-        )
-        print choices
-        choices = choices.filter(email__icontains=q)
+        try:
+            choices = User.objects.filter(
+                Q(is_superuser=False) & Q(is_active=True)
+            )
+            choices = choices.filter(email__icontains=q)
+        except Exception as e:
+            logger.error(
+            u'auto complete  {0} {1}'
+            u' '.format(q, str(e)))
+
         return choices
 
 
