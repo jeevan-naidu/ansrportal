bfd47b4f02269c4303b602cd06964939b513cb5d
balamurugan.rs <balamurugan.rs@ansrsource.com>
2017-02-17 16:57:31 +0530

    fixed rounding off issue

--- MyANSRSource/views.py
+++ MyANSRSource/views.py
@@ -1132,10 +1132,10 @@ def getTSDataList(request, weekstartDate, ansrEndDate, user_id=None):
         atDataList.append(atData.copy())
         atData.clear()
     if user_id:
-        total_list .append({'monday_total': monday_total, 'tuesday_total': tuesday_total,
-                            'wednesday_total': wednesday_total, 'thursday_total': thursday_total,
-                            'friday_total': friday_total, 'saturday_total': saturday_total,
-                            'sunday_total': sunday_total})
+        total_list .append({'monday_total': str(round(monday_total, 2)), 'tuesday_total': str(round(tuesday_total, 2)),
+                            'wednesday_total': str(round(wednesday_total, 2)), 'thursday_total': str(round(thursday_total, 2)),
+                            'friday_total': str(round(friday_total, 2)), 'saturday_total': str(round(saturday_total, 2)),
+                            'sunday_total': str(round(sunday_total, 2))})
     return {'tsData': tsDataList, 'atData': atDataList, 'total_list': total_list}
 
 
