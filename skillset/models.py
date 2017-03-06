abe45432a6f34e0c34d166cb3996481a562f2e34
janaki-jeevan <janaki.bs@ansrsource.com>
2017-03-06 13:10:24 +0530

    skillset

--- skillset/models.py
+++ skillset/models.py
@@ -3,35 +3,42 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 from employee.models import Employee
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
+SKILL_LEVEL=(
+    ('L1', 'Level 1'), ('L2', 'Level 2'), ('L3', 'Level 3')
+)
 
 # Create your models here.
 class Skill_Lists(models.Model):
     # User = models.ForeignKey(User, unique=True)
 
     skill_name = models.CharField(verbose_name="Skill Name", max_length=100)
-    level1 = models.CharField(verbose_name="Level1", max_length=500)
-    level2 = models.CharField(verbose_name="Level2", max_length=500)
-    level3 = models.CharField(verbose_name="Level3", max_length=500)
+    level1 = models.TextField(verbose_name="Level1", max_length=500)
+    level2 = models.TextField(verbose_name="Level2", max_length=500)
+    level3 = models.TextField(verbose_name="Level3", max_length=500)
 
     def __unicode__(self):
         return unicode(self.skill_name)
+
 
     class Meta:
         verbose_name = 'Skill List'
 
 
 class User_Skills(models.Model):
-    skill_id = models.ForeignKey(Skill_Lists, verbose_name="Skill ID")
+    
+    skill_name = models.ForeignKey(Skill_Lists, verbose_name="Skill Name", related_name="skills_name")
     employee_id = models.ForeignKey(Employee, verbose_name="Employee ID")
-    skill_name = models.CharField(max_length=500, verbose_name="Skill Name")
-    skill_type = models.CharField(max_length=500, verbose_name="Skill Type")
-    is_status = models.CharField(max_length=200, verbose_name="Is Status")
+    skill_level = models.CharField(choices=SKILL_LEVEL, max_length=10, verbose_name="Skill Level")
     createdon = models.DateTimeField(verbose_name="created Date", auto_now_add=True)
     updatedon = models.DateTimeField(verbose_name="Updated Date", auto_now_add=True)
 
     def __unicode__(self):
-        return u'{0},{1}'.format(self.skill_id, self.employee_id)
+        return '%s' % self.skill_name + " " + self.get_skill_level_display()
+
+
 
     class Meta:
         verbose_name = 'User Skills'
--- skillset/views.py
+++ skillset/views.py
@@ -3,12 +3,33 @@ from django.views.generic import View
 from django.shortcuts import render
 from django.utils import timezone
 from employee.models import Employee, Designation
+from CompanyMaster.models import Department
 from django.contrib import messages
 from django.contrib.auth.models import User
 from datetime import timedelta
 from datetime import date
+from models import Skill_Lists, User_Skills
 
 # Create your views here.
+# def SkillSet(request):
+
+#     if request.method == 'GET':
+#         # import ipdb;
+#         # ipdb.set_trace()
+#         user = request.user
+#         employee = Employee.objects.get(user_id=user.id)
+#         designation_all = Designation.objects.all()
+#         department  = Department.objects.all()
+#         skills = User_Skills.objects.filter(employee_id = employee)
+#         id = employee.idcard
+#         id = id[:-1]
+#         name = user.first_name +" "+ user.last_name
+#         designation = employee.designation.name
+        
+#         doj = employee.joined
+#         return render(request, 'skillset.html',{'skills':skills,'designation_all':designation_all,
+#             'id':id,'name':name,'designation':designation,'department':department,'doj':doj})
+
 def SkillSet(request):
 
     if request.method == 'GET':
@@ -17,9 +38,15 @@ def SkillSet(request):
         user = request.user
         employee = Employee.objects.get(user_id=user.id)
         designation_all = Designation.objects.all()
+        department  = Department.objects.all()
+        users = Employee.objects.all()
+        manager_filter = Employee.objects.filter(manager_id = user.manager_id)
+        skills = User_Skills.objects.filter(employee_id = employee)
         id = employee.idcard
+        id = id[:-1]
         name = user.first_name +" "+ user.last_name
         designation = employee.designation.name
-        department = employee.business_unit
+        
         doj = employee.joined
-        return render(request, 'skillset.html',{'designation_all':designation_all,'id':id,'name':name,'designation':designation,'department':department,'doj':doj})
+        return render(request, 'skillset.html',{'skills':skills,'designation_all':designation_all,
+            'id':id,'name':name,'designation':designation,'department':department,'doj':doj})
