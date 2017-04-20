674b11f44e61c76d6ba87f482934fc07bdb19842
balamurugan.rs <balamurugan.rs@ansrsource.com>
2017-04-20 16:52:02 +0530

    with QMS

---
+++ QMS/forms.py
@@ -0,0 +1,276 @@
+from django import forms
+from .models import *
+from django.contrib.auth.models import User
+from MyANSRSource.models import Project, Chapter, ProjectManager
+import datetime
+
+from dal import autocomplete
+
+
+class BaseAssessmentTemplateForm(forms.Form):
+    project = forms.ModelChoiceField(
+                queryset=Project.objects.filter(endDate__gte=datetime.date.today()),
+                widget=autocomplete.ModelSelect2(url='AutocompleteProjects', attrs={
+                 'data-placeholder': 'Project ',
+        }, ),
+                required=True, )
+
+    chapter = forms.ModelChoiceField(
+            queryset=Chapter.objects.all(),
+            widget=autocomplete.ModelSelect2(url='AutocompleteChapters', forward=('project',), attrs={
+             'data-placeholder': 'Chapter ',
+    }, ),
+            required=True, )
+
+    component = forms.ModelChoiceField(
+        queryset=ComponentMaster.objects.all(),
+        widget=autocomplete.ModelSelect2(url='AutoCompleteChapterSpecificComponent', forward=('project', 'chapter',),
+                                         attrs={
+                                             'data-placeholder': 'Component ',
+                                         }, ),
+        required=True, )
+
+    author = forms.ModelChoiceField(
+            queryset=User.objects.all(),
+            widget=autocomplete.ModelSelect2(url='AutoCompleteUserProjectSpecific', forward=('project', 'chapter',
+                                                                                             'component'), attrs={
+             'data-placeholder': 'Author ',
+    }, ),
+            required=True, )
+
+    class Meta:
+        model = QASheetHeader
+        fields = ('project', 'chapter', 'author', 'component', 'order_number')
+
+    def __init__(self, *args, **kwargs):
+        super(BaseAssessmentTemplateForm, self).__init__(*args, **kwargs)
+        # user = kwargs.pop('user', None)
+        # print user
+        # self.fields['project'].queryset = Project.objects.filter(
+        #     id__in=ProjectManager.objects.filter(
+        #         user=user
+        #     ).values('project')
+        # )
+        #     #
+        #     # project_id = self.fields['project'].initial\
+        #     #              or self.initial.get('project') \
+        #     #              or self.fields['project'].widget.value_from_datadict\
+        #     #                  (self.data, self.files, self.add_prefix('project'))
+        #     # if project_id:
+        #     #     try:
+        #     #         project_obj = Project.objects.get(id=int(project_id))
+        #     #     except:
+        #     #         project_obj = project_id
+        # #     self.fields['chapter'].queryset = Chapter.objects.filter(book=project_obj.book)
+        self.fields['project'].widget.attrs['class'] = " reset_field filter_form"
+        self.fields['chapter'].widget.attrs['class'] = "reset_field filter_form"
+        self.fields['author'].widget.attrs['class'] = " reset_field filter_form author_dropdown"
+        self.fields['project'].widget.attrs['required'] = True
+        self.fields['chapter'].widget.attrs['required'] = True
+        self.fields['author'].widget.attrs['required'] = True
+        self.fields['component'].widget.attrs['class'] = "filter_form reset_field"
+        self.fields['component'].widget.attrs['required'] = True
+
+
+class ChooseMandatoryTabsForm(BaseAssessmentTemplateForm):
+    qms_process_model = forms.ModelChoiceField(
+        queryset=QMSProcessModel.objects.all(),
+        widget=autocomplete.ModelSelect2(url='AutocompleteProcessModel', forward=('project',), attrs={
+            'data-placeholder': 'Process Model ',
+        }, ),
+        required=True, )
+    template = forms.ModelChoiceField(
+        queryset=TemplateMaster.objects.all(),
+        widget=autocomplete.ModelSelect2(url='AutocompleteTemplates', forward=('project', 'qms_process_model'), attrs={
+            'data-placeholder': 'Template ',
+        }, ),
+        required=True, )
+    # review_group = forms.ModelMultipleChoiceField(queryset=ReviewGroup.objects.all(),
+    #                                               widget=forms.CheckboxSelectMultiple(), required=False)
+    component = forms.ModelChoiceField(
+        queryset=ComponentMaster.objects.all(),
+        widget=autocomplete.ModelSelect2(url='AutocompleteComponents',
+                                         attrs={
+                                             'data-placeholder': 'Component',
+                                         }, ), required=True, )
+    # author = forms.ModelChoiceField(
+    #     queryset=User.objects.all(),
+    #     widget=autocomplete.ModelSelect2(url='AutoCompleteAssignUserProjectSpecific', forward=('project', 'chapter'),
+    #                                      attrs={
+    #                                          'data-placeholder': 'Author',
+    #                                      }, ),
+    #     required=True, )
+    # author = forms.ModelChoiceField(widget=forms.Select(), queryset=User.objects.none())
+    author = forms.ModelChoiceField(widget=forms.Select(), queryset=User.objects.filter(is_active=True))
+
+    def __init__(self, *args, **kwargs):
+        # print"init"
+        super(ChooseMandatoryTabsForm, self).__init__(*args, **kwargs)
+        # self.fields['author'].widget.attrs['class'] = 'author_dropdown'
+        self.fields['author'].widget.attrs['disabled'] = True
+
+        self.fields['qms_process_model'].widget.attrs['class'] = 'reset_field author_dropdown '
+        self.fields['template'].widget.attrs['class'] = 'reset_field author_dropdown'
+        self.fields['qms_process_model'].widget.attrs['required'] = True
+        self.fields['template'].widget.attrs['required'] = True
+
+        # project_id_field = self.fields['project'].initial \
+        #                    or self.initial.get('project') \
+        #                    or self.fields['project'].widget.value_from_datadict(self.data, self.files,
+        #                                                                         self.add_prefix('project'))
+        # print"project" , project_id_field
+        # if project_id_field:
+        #     print "if"
+        #     try:
+        #         project_obj = Project.objects.get(id=int(project_id_field))
+        #     except:
+        #         project_obj = project_id_field
+        #     self.fields['author'].queryset = ProjectTeamMember.objects.filter(project=project_obj,
+        #                                                                       member__is_active=True)
+        # else:
+        #     print "else"
+        #     self.fields['author'].queryset = User.objects.filter(is_active=True)
+        # # self.field_order['process_model', 'template', 'review_group', 'project', 'chapter', 'author']
+
+
+def review_report_base(template_id, project_obj, chapter_component_obj=None, request_obj=None, tab=None):
+    class ReviewReportForm(forms.Form):
+        review_item = forms.CharField(required=True,)
+        qms_id = forms.IntegerField(label="id",
+                                  required=False,
+                                  widget=forms.HiddenInput())
+        defect = forms.CharField(required=False, )
+        screen_shot_url = forms.CharField(required=False, )
+
+        clear_screen_shot = forms.BooleanField(label='clear screen shot', required=False, initial=False)
+
+        severity_type = forms.ModelChoiceField(widget=forms.Select(), required=True,
+                                               queryset=DefectTypeMaster.objects.all(), )
+        # defect_severity_level = forms.CharField()
+        # defect_classification = forms.CharField()
+
+        severity_level = forms.ModelChoiceField(widget=forms.Select(),
+                                                queryset=SeverityLevelMaster.objects.all(), required=False, )
+
+        defect_classification = forms.ModelChoiceField(widget=forms.Select(),
+                                                       queryset=DefectClassificationMaster.objects.all(),
+                                                       required=False,)
+
+        is_fixed = forms.ChoiceField(required=False, choices=fixed_status, )
+        fixed_by = forms.CharField(required=False,)
+        remarks = forms.CharField(required=False,)
+        screen_shot = forms.FileField(required=False,label="clear")
+        instruction = forms.CharField(required=False,)
+
+        class Meta:
+            model = ReviewReport
+            fields = ('review_item', 'defect', 'severity_type',  'is_fixed',
+                      'fixed_by', 'remarks', 'qms_id', 'screen_shot', 'instruction')
+            # choice_fields = ('severity_type','is_fixed','delete')
+
+        def __init__(self, *args, **kwargs):
+            super(ReviewReportForm, self).__init__(*args, **kwargs)
+            self.fields['severity_type'].widget.attrs['class'] = 'defect'
+            self.fields['severity_level'].widget.attrs['class'] = 'severity_level'
+            template_obj = ProjectTemplateProcessModel.objects.get(project=project_obj)
+            # defect_type_master_obj = DefectSeverityLevel.objects.filter(template=template_obj.template)
+            product_type = QMSProcessModel.objects.filter(id__in=
+                                                          ProjectTemplateProcessModel.objects.filter
+                                                          (project=project_obj).
+                                                          values_list('qms_process_model',
+                                                                      flat=True)).values_list('product_type',
+                                                                                              flat=True)[0]
+            if chapter_component_obj:
+                try:
+                    # print chapter_component_obj.id
+                    qa_obj = QASheetHeader.objects.filter(project=project_obj, chapter_component=chapter_component_obj,
+                                                          review_group_id=tab)[0]
+                    # print qa_obj.author_feedback_status,qa_obj.review_group_status
+                    # condition to prevent reviewer from editing
+
+                    if qa_obj.reviewed_by == request_obj.user:
+                        self.fields['remarks'].widget.attrs['readonly'] = True
+                        self.fields['is_fixed'].widget.attrs['disabled'] = True
+
+                    if qa_obj.review_group_status and not qa_obj.author_feedback_status and \
+                                    qa_obj.reviewed_by == request_obj.user:
+                        for field in self.fields:
+                            self.fields[field].widget.attrs['disabled'] = True
+                    # print qa_obj.review_group_status , qa_obj.author_feedback_status
+                    if qa_obj.review_group_status and qa_obj.author_feedback_status:
+                        for field in self.fields:
+                            self.fields[field].widget.attrs['disabled'] = True
+                    if not qa_obj.review_group_status and qa_obj.author_feedback_status:
+                        for field in self.fields:
+                            self.fields[field].widget.attrs['disabled'] = True
+                    if request_obj == qa_obj.author:
+                        self.fields["is_fixed"].widget.attrs['required'] = True
+                except Exception as e:
+                    # print "chapter_component_obj", str(e)
+                    pass
+
+                    # self.fields[field].widget.attrs['readonly'] = True
+            # print self.fields
+            # project_id_field = self.fields['project'].initial \
+            #                    or self.initial.get('project') \
+            #                    or self.fields['project'].widget.value_from_datadict(self.data, self.files,
+            #                                                                         self.add_prefix('project'))
+            # if project_id_field:
+            #
+            #     if project_id:
+            #         try:
+            #             project_obj = Project.objects.get(id=int(project_id_field))
+            #         except:
+            #             project_obj = project_id_field
+                    # print project_obj
+            #         self.fields['author'].queryset = ProjectTeamMember.objects.filter(project=project_obj).values(
+            #             'member_id')
+            # print self.fields['author'].queryset
+            # print defect_type_master_obj
+            # self.fields['severity_type'].queryset = DefectTypeMaster.objects.all()
+            self.fields['qms_id'].widget.attrs['class'] = "set-zero"
+            self.fields['qms_id'].widget.attrs['value'] = 0
+            # self.fields['severity_type'].queryset = DefectTypeMaster.objects.filter\
+            #     (id__in=defect_type_master_obj.severity_type)
+            # self.fields['defect_severity_level'].widget.attrs['disabled'] = True
+            self.fields['is_fixed'].widget.attrs['readonly'] = True
+            self.fields['fixed_by'].widget.attrs['readonly'] = True
+            # self.fields['severity_level'].queryset = SeverityLevelMaster.objects.all()
+            # self.fields['severity_level'].queryset = SeverityLevelMaster.objects.filter\
+            #     (id=defect_type_master_obj.severity_level)
+            self.fields['defect_classification'].queryset = DefectClassificationMaster.objects.all()
+            # self.fields['defect_classification'].queryset = DefectClassificationMaster.objects.filter\
+            #     (id=defect_type_master_obj.defect_classification)
+            # print DefectClassificationMaster.objects.filter(id__in=defect_type_master_obj)
+
+            self.fields['severity_level'].widget.attrs['disabled'] = True
+            self.fields['defect_classification'].widget.attrs['data-product_type'] = int(product_type)
+            self.fields['severity_type'].widget.attrs['data-product_type'] = int(product_type)
+            self.fields['defect_classification'].widget.attrs['class'] = "defect_classification"
+
+            if int(product_type) != 1:
+                self.fields['instruction'].widget.attrs['disabled'] = True
+                self.fields['defect_classification'].widget.attrs['disabled'] = True
+
+            else:
+                pass
+                # print "pt", product_type, type(product_type)
+            # self.fields['author'].widget.attrs['class'] = 'author_dropdown'
+
+            # print defect_type_master_obj
+            # self.fields['severity_type'].queryset = DefectTypeMaster.filter(id__in=[defect_type_master_obj])
+            # severity_type_id = self.fields['severity_type'].initial\
+            #              or self.initial.get('severity_type') \
+            #              or self.fields['severity_type'].widget.value_from_datadict\
+            #                  (self.data, self.files, self.add_prefix('severity_type'))
+            # if severity_type_id:
+            #     severity_type_obj = defect_type_master_obj.filter(severity_type=severity_type_id)
+            #
+            #     self.fields['severity_level'].queryset = SeverityLevelMaster.objects.filter\
+            #     (id=defect_type_master_obj.severity_level)
+            #     self.fields['defect_classification'].queryset = DefectClassificationMaster.objects.filter\
+            #         (id__in=severity_type_obj.defect_classification)
+
+    return ReviewReportForm
+
+
---
+++ QMS/models.py
@@ -0,0 +1,222 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+from MyANSRSource.models import Project, Chapter
+from simple_history.models import HistoricalRecords
+
+fixed_status = (('', '------'), ('fixed', 'Fixed'), ('fix_not_required', 'Fix Not Required'))
+team_choices = ((0, 'assessment'), ('1', 'learning'))
+
+
+class TimeStampAbstractModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='%(class)s_created_by', blank=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(User, related_name='%(class)s_updated_by', blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class NameMasterAbstractModel(models.Model):
+
+    name = models.CharField(max_length=100, unique=True)
+    is_active = models.BooleanField(blank=False, default=True, verbose_name="Is Active?")
+
+    class Meta:
+        abstract = True
+
+    def __unicode__(self):
+        """ return unicode strings """
+        return '%s' % self.name
+
+
+class TemplateMaster(NameMasterAbstractModel, TimeStampAbstractModel):
+    actual_name = models.CharField(max_length=100, unique=True)
+    pass
+
+
+class QMSProcessModel(NameMasterAbstractModel, TimeStampAbstractModel):
+    product_type = models.CharField(max_length=50, blank=True, null=True, choices=team_choices, verbose_name="Product",
+                                    default=team_choices[0][0])
+    pass
+
+
+class ProjectTemplateProcessModel(TimeStampAbstractModel):
+
+    project = models.ForeignKey(Project)
+    template = models.ForeignKey(TemplateMaster)
+    lead_review_status = models.BooleanField(
+        default=False,
+        null=False,
+        verbose_name=" Is Lead Review Completed "
+    )
+    lead_review_feedback = models.TextField(
+        blank=True, null=True,
+        verbose_name=" Lead Review Feedback "
+    )
+    qms_process_model = models.ForeignKey(QMSProcessModel)
+
+    class Meta:
+        ordering = ['lead_review_status']
+
+    def __unicode__(self):
+        """ return unicode strings """
+        return '%s' % (str(self.project) + " - " + str(self.template))
+
+
+class DefectTypeMaster(NameMasterAbstractModel, TimeStampAbstractModel):
+
+    pass
+
+
+class SeverityLevelMaster(NameMasterAbstractModel, TimeStampAbstractModel):
+    penalty_count = models.DecimalField(blank=False, decimal_places=1, max_digits=2, verbose_name="Penalty Count",
+                                        null=False)
+    pass
+
+
+class DefectClassificationMaster(NameMasterAbstractModel, TimeStampAbstractModel):
+
+    pass
+
+
+class ReviewMaster(NameMasterAbstractModel):
+
+    pass
+
+
+class WorkPacketMaster(NameMasterAbstractModel, TimeStampAbstractModel):
+    pass
+
+
+class ComponentMaster(NameMasterAbstractModel, TimeStampAbstractModel):
+    pass
+
+
+class ReviewGroup(models.Model):
+    name = models.CharField(max_length=100)
+    review_master = models.ForeignKey(ReviewMaster)
+    alias = models.CharField(max_length=20, blank=True, null=True, verbose_name="alias/display name for review group",
+                             editable=False)
+
+    def __unicode__(self):
+        """ return unicode strings """
+        return '%s' % str(self.alias)
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            try:
+                last_pk = ReviewGroup.objects.filter(name__icontains=self.name).count()
+                last_pk += 1
+                self.alias = ''.join((self.name, "(", str(last_pk), ")"))
+            except:
+                self.alias = ''.join((self.name, "(1)"))
+        else:
+            alias = self.alias.split("(")
+            self.alias = ''.join((self.name, "(", str(alias[1])))
+
+        super(ReviewGroup, self).save(*args, **kwargs)
+
+
+class DefectSeverityLevel(TimeStampAbstractModel):
+    severity_type = models.ForeignKey(DefectTypeMaster)
+    severity_level = models.ForeignKey(SeverityLevelMaster)
+    defect_classification = models.ForeignKey(DefectClassificationMaster)
+    product_type = models.CharField(max_length=50, blank=True, null=True, choices=team_choices, verbose_name="Product"
+                                    , default=team_choices[0][0])
+    is_active = models.BooleanField(blank=False, default=True, verbose_name="Is Active?")
+
+    class Meta:
+        unique_together = (("severity_type", "severity_level", "defect_classification"),)
+
+    def __unicode__(self):
+        """ return unicode strings """
+        return '%s' % self.id
+        return '%s' % (str(self.severity_type)+": " + str(self.severity_level) + ": "
+                       + str(self.defect_classification))
+
+
+class DSLTemplateReviewGroup(TimeStampAbstractModel):
+    defect_severity_level = models.ForeignKey(DefectSeverityLevel)
+    template = models.ForeignKey(TemplateMaster)
+    review_master = models.ForeignKey(ReviewMaster)
+    is_active = models.BooleanField(blank=False, default=True, verbose_name="Is Active?")
+
+    def __unicode__(self):
+        """ return unicode strings """
+        return '%s' % self.id
+
+
+class ChapterComponent(TimeStampAbstractModel):
+    chapter = models.ForeignKey(Chapter, blank=False, verbose_name="Chapter/Subtitle", null=False)
+    component = models.ForeignKey(ComponentMaster, blank=False, verbose_name="Component", null=False)
+
+    def __unicode__(self):
+        """ return unicode strings """
+        return '%s' % self.id
+
+
+class QASheetHeader(TimeStampAbstractModel):
+    project = models.ForeignKey(Project)
+    chapter = models.ForeignKey(Chapter, blank=False,
+                                verbose_name="Chapter/Subtitle", null=True)
+    chapter_component = models.ForeignKey(ChapterComponent, blank=True, verbose_name="Chapter&Component", null=True)
+
+    work_packet = models.ForeignKey(WorkPacketMaster, verbose_name="work packet (output)", blank=True, null=True, )
+    count = models.IntegerField(verbose_name="work packet count", default=0)
+    author = models.ForeignKey(User, related_name='QASheetHeader_author', blank=True, null=True,)
+    review_group = models.ForeignKey(ReviewGroup)
+    reviewed_by = models.ForeignKey(User, related_name='QASheetHeader_reviewed_by')
+    review_group_feedback = models.CharField(max_length=100, blank=True, null=True,)
+    review_group_status = models.BooleanField(
+        default=False,
+        null=False,
+        verbose_name=" Is Review Completed "
+    )
+    author_feedback = models.CharField(max_length=100, blank=True, null=True,)
+    author_feedback_status = models.BooleanField(
+        default=False,
+        null=False,
+        verbose_name=" Is author feedback Completed "
+    )
+    order_number = models.IntegerField(blank=False,
+                                       verbose_name="Review tab Order", null=False)
+    history = HistoricalRecords()
+
+    def __unicode__(self):
+        """ return unicode strings """
+        return '%s' % (str(self.project) +
+                       " : " + str(self.review_group) +
+                       " : " + str(self.chapter)
+                       )
+
+
+class ReviewReport(TimeStampAbstractModel):
+    QA_sheet_header = models.ForeignKey(QASheetHeader)
+    review_item = models.CharField(max_length=100)
+    defect = models.TextField()
+    instruction = models.TextField(blank=True, null=True, verbose_name="Instruction to fix")
+    screen_shot = models.FileField(upload_to='qms', blank=True, null=True, verbose_name="upload screen shot")
+    defect_severity_level = models.ForeignKey(DefectSeverityLevel)
+    is_fixed = models.CharField(max_length=50, blank=True, null=True, choices=fixed_status, verbose_name="Is Fixed?")
+    fixed_by = models.ForeignKey(User, related_name='reviewer_report_fixed_by', blank=True, null=True,)
+    remarks = models.TextField(blank=True, null=True,)
+    is_active = models.BooleanField(blank=False, default=True, verbose_name="Is Active?")
+    history = HistoricalRecords()
+
+    def __unicode__(self):
+        """ return unicode strings """
+        return '%s' % str(self.QA_sheet_header)
+
+
+class TemplateProcessReview(TimeStampAbstractModel):
+    template = models.ForeignKey(TemplateMaster)
+    qms_process_model = models.ForeignKey(QMSProcessModel)
+    review_group = models.ForeignKey(ReviewGroup)
+    is_mandatory = models.BooleanField(default=False, verbose_name="Is Mandatory")
+
+    def __unicode__(self):
+        """ return unicode strings """
+        return '%s' % self.id
