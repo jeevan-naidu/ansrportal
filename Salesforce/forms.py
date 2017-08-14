6b9bc92b99616372a50c0ebe63b6be7835e9954b
janaki-jeevan <janaki.bs@ansrsource.com>
2017-08-14 16:19:17 +0530

    Salesforce changes

--- Salesforce/forms.py
+++ Salesforce/forms.py
@@ -1,6 +1,41 @@
 from django import forms
-
+from models import SalesforceData
+from bootstrap3_datetime.widgets import DateTimePicker
+dateTimeOption = {"format": "MM/DD/YYYY", "pickTime": False}
 
 class UploadSalesforceDataForm(forms.Form):
     salesforce_data_file = forms.FileField(required=True, help_text="Please use semicolon delimited csv only")
-    salesforce_data_file.widget.attrs = {'class':'filestyle', 'data-buttonBefore':'true', 'data-iconName':'glyphicon glyphicon-paperclip'}
+    salesforce_data_file.widget.attrs = {'class': 'filestyle', 'data-buttonBefore': 'true',
+                                         'data-iconName': 'glyphicon glyphicon-paperclip'}
+
+
+class SalesforceDataForm(forms.ModelForm):
+
+    opportunity_number = forms.CharField(max_length=8, widget=forms.TextInput(attrs={'class': 'input-sm form-control width-40',
+                                                                              'required': 'True',
+                                                                              'data-error': 'Enter the SFID number'}))
+    opportunity_name = forms.CharField(max_length=50, widget=forms.TextInput(
+        attrs={'class': 'input-sm form-control width-40', 'required': 'True'}))
+    value = forms.CharField(max_length=50, widget=forms.TextInput(
+        attrs={'class': 'input-sm form-control width-40', 'required': 'True'}))
+    estimate_start_date = forms.DateField(label=('Estimated Start Date'),
+                                widget=DateTimePicker(options=dateTimeOption, attrs = {'class': 'form-control input-sm', 'required':''}))
+    estimate_end_date = forms.DateField(label=('Estimated End Date'),
+                                widget=DateTimePicker(options=dateTimeOption, attrs = {'class': 'form-control input-sm', 'required':''}))
+    planned_start_date = forms.DateField(label=('Planned Start Date'),
+                                 widget=DateTimePicker(options=dateTimeOption,
+                                                       attrs={'class': 'form-control input-sm', 'required': ''}))
+    planned_end_date = forms.DateField(label=('Planned End Date'),
+                               widget=DateTimePicker(options=dateTimeOption,
+                                                     attrs={'class': 'form-control input-sm', 'required': ''}))
+    customer_contact = forms.CharField(max_length=50, widget=forms.TextInput(
+        attrs={'class': 'input-sm form-control width-40', 'required': 'True'}))
+    account_name = forms.CharField(max_length=50, widget=forms.TextInput(
+        attrs={'class': 'input-sm form-control width-40', 'required': 'True'}))
+
+
+    class Meta:
+        model = SalesforceData
+
+        fields = ['opportunity_number', 'opportunity_name', 'value', 'estimate_start_date',
+                  'estimate_end_date', 'planned_start_date', 'planned_end_date','customer_contact', 'account_name']
--- Salesforce/views.py
+++ Salesforce/views.py
@@ -1,25 +1,29 @@
 from django.shortcuts import render
 from django.views.generic import View
-from forms import UploadSalesforceDataForm
+from MyANSRSource.models import Project
+from CompanyMaster.models import BusinessUnit
+from forms import UploadSalesforceDataForm, SalesforceDataForm
 from models import SalesforceData
-# Create your views here.
+from django.contrib import messages
 import csv
 import re
 import datetime
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
 
+
 def CheckAccessPermissions(request):
-    if not not request.user.groups.filter(name=settings.SALESFORCE_ADMIN_GROUP_NAME).exists() or\
-    not request.user.groups.filter(name=settings.MANAGER).exists():
-            raise PermissionDenied("Sorry, you don't have permission to access this feature")
+    if not not request.user.groups.filter(name=settings.SALESFORCE_ADMIN_GROUP_NAME).exists() or \
+            not request.user.groups.filter(name=settings.MANAGER).exists():
+        raise PermissionDenied("Sorry, you don't have permission to access this feature")
+
 
 class UploadSalesforceDataView(View):
     """ """
 
     def get(self, request):
         # CheckAccessPermissions(request)
-        context_data = {'add': True, 'record_added': False, 'form': None, 'salesforce_data_list':[]}
+        context_data = {'add': True, 'record_added': False, 'form': None, 'salesforce_data_list': []}
         form = UploadSalesforceDataForm()
         context_data['salesforce_data_list'] = SalesforceData.objects.all().order_by('-created_date')
         context_data['form'] = form
@@ -27,67 +31,110 @@ class UploadSalesforceDataView(View):
 
     def post(self, request):
         # CheckAccessPermissions(request)
-        context_data = {'record_added': False, 'form': None, 'errors_list':[], 'errors': False,
-                        'salesforce_data_list': [],'exception_type': None, 'exception': None, 'error_at_line': None}
+        context_data = {'record_added': False, 'form': None, 'errors_list': [], 'errors': False,
+                        'salesforce_data_list': [], 'exception_type': None, 'exception': None, 'error_at_line': None}
         context_data['salesforce_data_list'] = SalesforceData.objects.all().order_by('-created_date')
-        
+
         form = UploadSalesforceDataForm(request.POST, request.FILES)
         context_data['form'] = form
-        
+
         if form.is_valid():
             data_file = request.FILES.get('salesforce_data_file', '')
-            
+
             db_columns_list = ['opportunity_number', 'opportunity_name', 'business_unit', 'client_rep',
                                'account_name', 'amount', 'probability_(%)', 'expected_project_start_date',
                                'expected_project_end_date', 'estimated_close_date', 'stage']
-            
-            reader  = csv.reader(data_file , delimiter = ';')
+
+            reader = csv.reader(data_file, delimiter=';')
             headers_list = reader.next()
             headers_list = [i.lower().replace(" ", "_") for i in headers_list]
-            
+
             for index, row in enumerate(reader):
                 try:
-                    temp_var = re.findall("[a-zA-Z]|[0-9]", row[0])  # to check for empty rows in csv,empty row will not contain any characters so exlude it
+                    temp_var = re.findall("[a-zA-Z]|[0-9]", row[
+                        0])  # to check for empty rows in csv,empty row will not contain any characters so exlude it
                     if temp_var:
                         row_dict = dict(zip(headers_list, row))
-                        
-                        start_date = row_dict.get('expected_project_start_date', None)
-                        if start_date:
-                            start_date = datetime.datetime.strptime(start_date, '%m/%d/%Y').date()
+
+                        estimate_start_date = row_dict.get('expected_project_start_date', None)
+                        if estimate_start_date:
+                            estimate_start_date = datetime.datetime.strptime(estimate_start_date, '%m/%d/%Y').date()
                         else:
-                            start_date = None
-                            
-                        end_date = row_dict.get('expected_project_end_date', None)
-                        if end_date:
-                            end_date = datetime.datetime.strptime(end_date, '%m/%d/%Y').date()
+                            estimate_start_date = None
+
+                        estimate_end_date = row_dict.get('expected_project_end_date', None)
+                        if estimate_end_date:
+                            estimate_end_date = datetime.datetime.strptime(estimate_end_date, '%m/%d/%Y').date()
                         else:
-                            end_date = None
-                            
+                            estimate_end_date = None
+
                         try:
-                            obj = SalesforceData.objects.get(opportunity_number=int(row_dict.get('opportunity_number', '').strip()))
+                            obj = SalesforceData.objects.get(
+                                opportunity_number=int(row_dict.get('opportunity_number', '').strip()))
                         except:
                             obj = SalesforceData()
                             obj.opportunity_number = int(row_dict.get('opportunity_number', '').strip())
-                        
+
                         obj.opportunity_name = row_dict.get('opportunity_name', '').strip()
                         obj.business_unit = row_dict.get('business_unit', '').strip()
                         obj.customer_contact = row_dict.get('client_rep', '').strip()
                         obj.account_name = row_dict.get('account_name', '').strip()
                         obj.value = row_dict.get('amount', '').strip()
                         obj.probability = row_dict.get('probability_(%)', '').strip()
-                        obj.start_date = start_date
-                        obj.end_date = end_date
+                        obj.estimate_start_date = estimate_start_date
+                        obj.estimate_end_date = estimate_end_date
                         obj.status = row_dict.get('stage', '').strip()
                         obj.save()
                 except Exception as e:
 
                     context_data['errors'] = True
-                    context_data['errors_list'].append("\n\nLine:" + str(index+2) + " - Exception: " +
-                               str(e))
+                    context_data['errors_list'].append("\n\nLine:" + str(index + 2) + " - Exception: " +
+                                                       str(e))
                     context_data['error_at_line'] = index + 2
                     # return render(request, 'upload_salesforce_data.html', context_data)
-                
+
         return render(request, 'upload_salesforce_data.html', context_data)
-            
-           
-            
+
+
+def contract_details(request):
+    if request.method == 'GET':
+        context = {"form": ""}
+        form = SalesforceDataForm()
+        context["form"] = form
+        return render(request, 'contract_details.html', context)
+    if request.method == 'POST':
+        context = {"form": ""}
+        user = request.user
+        form = SalesforceDataForm(request.POST)
+        if form.is_valid():
+            opportunity_number = form.cleaned_data['opportunity_number']
+            opportunity_name = form.cleaned_data['opportunity_name']
+            value = form.cleaned_data['value']
+            estimate_start_date = form.cleaned_data['estimate_start_date']
+            estimate_end_date = form.cleaned_data['estimate_end_date']
+            planned_start_date = form.cleaned_data['planned_start_date']
+            planned_end_date = form.cleaned_data['planned_end_date']
+            customer_contact = form.cleaned_data['customer_contact']
+            account_name = form.cleaned_data['account_name']
+            if estimate_start_date > estimate_end_date or planned_start_date > planned_end_date:
+                messages.error(request, 'Sorry, End date is greater than start date')
+                context['form'] = SalesforceDataForm(request.POST)
+                return render(request, "contract_details.html", context)
+            SalesforceData(user_id=user.id,
+                           opportunity_number=opportunity_number,
+                           # business_unit = business_unit,
+                           opportunity_name=opportunity_name,
+                           value=value,
+                           estimate_start_date=estimate_start_date,
+                           estimate_end_date=estimate_end_date,
+                           planned_start_date=planned_start_date,
+                           planned_end_date=planned_end_date,
+                           customer_contact=customer_contact,
+                           account_name=account_name).save()
+            messages.success(request, 'Thanks')
+            context['form'] = SalesforceDataForm()
+            return render(request, 'contract_details.html', context)
+        else:
+            context['form'] = SalesforceDataForm(request.POST)
+            context['form_errors'] = form.errors
+        return render(request, 'contract_details.html', context)
