bc8a8847bd0c0a1171ef163b654041a3f48a9a01
janaki-jeevan <janaki.bs@ansrsource.com>
2017-08-23 13:29:03 +0530

    milsetone calc

--- MyANSRSource/forms.py
+++ MyANSRSource/forms.py
@@ -865,7 +865,7 @@ class CloseProjectMilestoneFormDelivery(forms.ModelForm):
         self.fields['amount'].widget.attrs['class'] = \
             "milestone-item-amount d-item input-item form-control"
         self.fields['closed'].widget.attrs['class'] = "form-control"
-        self.fields['amount'].widget.attrs['class'] = "milestone_delivery"
+        self.fields['amount'].widget.attrs['class'] = "milestone-item-amount milestone_delivery"
 
 
 # Project Flag Form
