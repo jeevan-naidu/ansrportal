de276266847920d4de8f1a01a46973d079a1e855
janaki-jeevan <janaki.bs@ansrsource.com>
2017-08-30 11:33:43 +0530

    skillset changes

--- skillset/urls.py
+++ skillset/urls.py
@@ -5,4 +5,7 @@ from skillset import views
 urlpatterns = [
 
     url(r'^$', login_required(views.SkillSet)),
+    url(r'^skill_assign/$', login_required(views.SkillSet_assign)),
+    url(r'^skill_detail/$', login_required(views.skill_detail)),
+    url(r'^skill_add/$', login_required(views.skill_add)),
     ]
--- skillset/views.py
+++ skillset/views.py
@@ -8,10 +8,149 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from datetime import timedelta
 from datetime import date
+import datetime
 from models import Skill_Lists, User_Skills
-import xlwt
 from django.http import JsonResponse
 import json
+import ast
+
+def skill_add(request):
+    json_skill = json.loads(request.body)
+    details = ast.literal_eval(json_skill['json_skill'])
+    for skills_name, skills_level in zip(details['skills_name'], details['skills_level']):
+        user_skill_detail = User_Skills.objects.all()
+        last_elem = len(user_skill_detail) - 1
+        sid = user_skill_detail[last_elem]
+        new_sid = sid.sid + 1
+        user_skills = User_Skills(emp_mid = details['id'],sid = new_sid, skills_name=skills_name, skills_type = skills_level)
+        user_skills.save()
+
+    return render(request,'skillassign.html', {})
+
+def skill_detail(request):
+    id = request.GET.get('id')
+    employee = Employee.objects.get(employee_assigned_id=id)
+    user_skills = Skill_Lists.objects.all()
+    user_details = {"name": "", "deisgnation": "", "department": "", "id": "", "doj": "", "skills": ""}
+    try:
+        skillset = User_Skills.objects.filter(emp_mid=id).values('skills_name','skills_type')
+        skills_list = []
+        skills_dict = {'skills_name': '', 'skills_type': ''}
+        for skill in skillset:
+            skills_dict['skills_name'] = skill['skills_name']
+            skills_dict['skills_type'] = skill['skills_type']
+            skills_list.append(skills_dict)
+            skills_dict = {'skills_name': '', 'skills_type': ''}
+    except User_Skills.DoesNotExist:
+        skills_list = None
+    try:
+        depart = EmployeeCompanyInformation.objects.get(employee_id=id)
+        dept = depart.department.name
+    except:
+        dept = ""
+    user_details['name'] = employee.user.first_name + ' ' + employee.user.last_name
+    user_details['designation'] = employee.designation.name
+    user_details['department'] = dept
+    id = employee.idcard
+    user_details['id'] = id[:-1]
+    user_details['doj'] = employee.joined
+    user_details['skills'] = skills_list
+    return render(request, 'user_skills.html', {'user_details':user_details, 'user_skills':user_skills})
+
+def SkillSet_assign(request):
+    if request.method == 'GET':
+        user = request.user
+        if request.user.groups.filter(name__in=['myansrsourceHR']).exists() or request.user.is_superuser == True:
+            designation_all = []
+            department = []
+            skills_all = []
+            reportee = Employee.objects.select_related('designation', 'user').filter(user__is_active=True)
+            lists = []
+            user_details = {"name": "", "deisgnation": "", "department": "", "id": "", "doj": "", "skills": ""}
+            for employee in reportee:
+                designation_all.append(employee.designation.name)
+                designation_all = sorted(set(designation_all))
+                try:
+                    skillset = User_Skills.objects.filter(emp_mid=employee.employee_assigned_id).values('skills_name','skills_type')
+                    skills_list = []
+                    skills_dict = {'skills_name':'','skills_type':''}
+                    for skill in skillset:
+                        skills_dict['skills_name'] = skill['skills_name']
+                        skills_dict['skills_type'] = skill['skills_type']
+                        skills_list.append(skills_dict)
+                        skills_dict = {'skills_name':'','skills_type':''}
+                    if skills_list:
+                        for skills_user in skills_list:
+                            skills_all.append(skills_user['skills_name'])
+                    skills_all = sorted(set(skills_all))
+                except User_Skills.DoesNotExist:
+                    skills_list = None
+                try:
+                    depart = EmployeeCompanyInformation.objects.get(employee_id=employee.employee_assigned_id)
+                    dept = depart.department.name
+                except:
+                    dept = ""
+                department.append(dept)
+                department = sorted(set(department))
+                user_details['name'] = employee.user.first_name + ' ' + employee.user.last_name
+                user_details['designation'] = employee.designation.name
+                user_details['department'] = dept
+                id = employee.idcard
+                user_details['id'] = id[:-1]
+                user_details['doj'] = employee.joined
+                user_details['skills'] = skills_list
+                lists.append(user_details)
+                user_details = {"name": "", "deisgnation": "", "department": "", "id": "", "doj": "",  "skills": ""}
+
+        elif request.user.groups.filter(name__in=['myansrsourcePM']).exists():
+            employee = Employee.objects.get(user_id=user.id)
+            designation_all = []
+            department = []
+            skills_all = []
+            mgrid = Employee.objects.get(user_id=request.user.id)
+            reportee_business_unit = BusinessUnit.objects.filter(new_bu_head=mgrid.user_id)
+            if not reportee_business_unit:
+                reportee = Employee.objects.filter(manager_id=mgrid, user__is_active=True)
+                lists = []
+                user_details = {"name": "", "deisgnation": "", "department": "", "id": "", "doj": "", "skills": ""}
+                for employee in reportee:
+                    designation_all.append(employee.designation.name)
+                    designation_all = sorted(set(designation_all))
+                    try:
+                        skillset = User_Skills.objects.filter(emp_mid=employee.employee_assigned_id).values(
+                            'skills_name', 'skills_type')
+                        skills_list = []
+                        skills_dict = {'skills_name': '', 'skills_type': ''}
+                        for skill in skillset:
+                            skills_dict['skills_name'] = skill['skills_name']
+                            skills_dict['skills_type'] = skill['skills_type']
+                            skills_list.append(skills_dict)
+                            skills_dict = {'skills_name': '', 'skills_type': ''}
+                        if skills_list:
+                            for skills_user in skills_list:
+                                skills_all.append(skills_user['skills_name'])
+                        skills_all = sorted(set(skills_all))
+                    except User_Skills.DoesNotExist:
+                        skills_list = None
+                    try:
+                        depart = EmployeeCompanyInformation.objects.get(employee_id=employee.employee_assigned_id)
+                        dept = depart.department.name
+                    except:
+                        dept = ""
+                    department.append(dept)
+                    department = sorted(set(department))
+                    user_details['name'] = employee.user.first_name + ' ' + employee.user.last_name
+                    user_details['designation'] = employee.designation.name
+                    user_details['department'] = dept
+                    user_details['id'] = employee.employee_assigned_id
+                    user_details['doj'] = employee.joined
+                    user_details['skills'] = skills_list
+                    lists.append(user_details)
+                    user_details = {"name": "", "deisgnation": "", "department": "", "id": "", "doj": "",
+                                        "skills": ""}
+
+
+        return render(request, 'skillassign.html', {'lists':lists})
 
 def SkillSet(request):
     if request.method == 'GET':
