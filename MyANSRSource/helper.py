142216ac086925dde9942fa882066483cbde2f11
pupul <pupul.ranjan@ansrsource.com>
2017-09-13 13:58:56 +0530

    Unique together and project list for Business unit head

--- MyANSRSource/helper.py
+++ MyANSRSource/helper.py
@@ -12,6 +12,9 @@ def get_my_project_list(requestee, pmflag=0):
     # ipdb.set_trace()
     pm_delegate = myansr_model.ProjectDetail.objects.filter(pmDelegate_id=requestee.id)
     delivery_manager = myansr_model.ProjectDetail.objects.filter(deliveryManager_id=requestee.id)
+    bu_head = company_model.BusinessUnit.objects.filter(new_bu_head=requestee)
+    if bu_head:
+        return myansr_model.Project.objects.filter(bu__in=bu_head).values('id')
     if delivery_manager:
         return myansr_model.ProjectDetail.objects.filter(deliveryManager_id=requestee.id).values('project_id')
     if delivery_manager and requestee.is_superuser:
@@ -25,15 +28,12 @@ def get_my_project_list(requestee, pmflag=0):
     if pmflag:
         return myansr_model.ProjectManager.objects.filter(user=requestee).values('project__id')
 
-    bu_head = company_model.BusinessUnit.objects.filter(new_bu_head=requestee)
+
     acc_mgmt = company_model.Customer.objects.filter(Q(Crelation=requestee) | Q(Cdelivery=requestee))
     pm = myansr_model.ProjectManager.objects.filter(user=requestee)
-
-
     if requestee.is_superuser:
         return myansr_model.Project.objects.all().values('id')
-    if bu_head:
-        return myansr_model.Project.objects.filter(bu__in=bu_head).values('id')
+
     elif acc_mgmt:
         return myansr_model.Project.objects.filter(customer__in=acc_mgmt).values('id')
     elif pm:
