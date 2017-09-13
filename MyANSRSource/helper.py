934b076c364cbd3694f06c3e2569e4131ec4e1cd
janaki-jeevan <janaki.bs@ansrsource.com>
2017-09-13 15:38:59 +0530

    pm delegate fix

--- MyANSRSource/helper.py
+++ MyANSRSource/helper.py
@@ -8,21 +8,22 @@ def get_my_project_list(requestee, pmflag=0):
     :param requestee:
     :return Eligible Project's ID as list:
     """
-    # import ipdb;
-    # ipdb.set_trace()
     pm_delegate = myansr_model.ProjectDetail.objects.filter(pmDelegate_id=requestee.id)
     delivery_manager = myansr_model.ProjectDetail.objects.filter(deliveryManager_id=requestee.id)
     bu_head = company_model.BusinessUnit.objects.filter(new_bu_head=requestee)
     if bu_head:
         return myansr_model.Project.objects.filter(bu__in=bu_head).values('id')
-    if delivery_manager:
-        return myansr_model.ProjectDetail.objects.filter(deliveryManager_id=requestee.id).values('project_id')
-    if delivery_manager and requestee.is_superuser:
-        return myansr_model.ProjectManager.objects.values('project__id')
-    if pm_delegate:
-        return myansr_model.ProjectDetail.objects.filter(pmDelegate_id=requestee.id).values('project_id')
-    if pm_delegate and requestee.is_superuser:
-        return myansr_model.ProjectManager.objects.values('project__id')
+    pmdelegate_id = myansr_model.ProjectDetail.objects.filter(pmDelegate_id=requestee.id).values('pmDelegate_id')
+    if requestee.id == pmdelegate_id:
+        if pm_delegate:
+            return myansr_model.ProjectDetail.objects.filter(pmDelegate_id=requestee.id).values('project_id')
+        if pm_delegate and requestee.is_superuser:
+            return myansr_model.ProjectManager.objects.values('project__id')
+    if requestee.id == delivery_manager:
+        if delivery_manager:
+            return myansr_model.ProjectDetail.objects.filter(deliveryManager_id=requestee.id).values('project_id')
+        if delivery_manager and requestee.is_superuser:
+            return myansr_model.ProjectManager.objects.values('project__id')
     if pmflag and requestee.is_superuser:
         return myansr_model.ProjectManager.objects.values('project__id')
     if pmflag:
