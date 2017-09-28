f5c4ae92419aedbb65c382c76e6daca895eeca6c
pupul <pupul.ranjan@ansrsource.com>
2017-09-28 16:22:58 +0530

    prod settings

--- timetracker/settings.py
+++ timetracker/settings.py
@@ -106,7 +106,7 @@ SECRET_KEY = 'pi3q*!l_+$+vd&3&v_zb*yt6mmi=h*25o#6!q5!aca=j_)&3yd'
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'portal.ansrsource.com', 'stage.ansrsource.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'portal.ansrsource.com']
 
 # When CRSF failurers happen we just ask them to relogin using our own template
 CSRF_FAILURE_VIEW = 'MyANSRSource.views.csrf_failure'
@@ -150,10 +150,10 @@ INSTALLED_APPS = [
     'ExitApp',
     'Invoice',
     'skillset',
+    'TimeInOffice',
+    'LaptopAvail',
     'QMS',
     'simple_history',
-    'TimeInOffice',
-    'LaptopAvail'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -166,10 +166,10 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'session_security.middleware.SessionSecurityMiddleware',
-    'simple_history.middleware.HistoryRequestMiddleware',
     'GrievanceAdmin.middleware.grievanceadminmiddleware.GrievancePermissionCheckMiddleware',
-    'QMS.middleware.qms_middleware.QMSPermissionCheckMiddleware',
     'ExitApp.middleware.exitappmiddleware.ExitappPermissionCheckMiddleware',
+    'simple_history.middleware.HistoryRequestMiddleware',
+    'QMS.middleware.qms_middleware.QMSPermissionCheckMiddleware',
 ]
 # Overriding Default T_C_P with new T_C_p
 TEMPLATES = [
@@ -216,14 +216,15 @@ ROOT_URLCONF = 'timetracker.urls'
 WSGI_APPLICATION = 'timetracker.wsgi.application'
 
 
-
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "myansrsource",
         "USER": "root",
-        "PASSWORD": "root",
+        "PASSWORD": "mysqlroot",
         "HOST": "localhost",
         "PORT": "3306",
         },
@@ -343,7 +344,7 @@ FEED_DELIMITER = ","
 EXTERNAL_PROJECT_NOTIFIERS = ['sanjay.kunnath@ansrsource.com']
 
 # New Joinee Notifiers
-NEW_JOINEE_NOTIFIERS = ['shalini.bhagat@ansrsource.com']
+NEW_JOINEE_NOTIFIERS = ['hrhelpdesk@ansrsource.com']
 
 # Grappelli Customizations
 GRAPPELLI_ADMIN_TITLE = 'myansrsource administration'
@@ -361,9 +362,9 @@ BACKUPDIR = '/www/MyANSRSource/ansr-timesheet/backup'
 MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
 MEDIA_URL = '/media/'
 
-GRIEVANCES_ADMIN_EMAIL = "shalini.bhagat@ansrsource.com"
+GRIEVANCES_ADMIN_EMAIL = "HR.Helpdesk@ansrsource.com"
 BOOKING_ROOM_ADMIN = "BookingRoomAdmin"
-LEAVE_ADMIN_EMAIL = ['shalini.bhagat@ansrsource.com']
+LEAVE_ADMIN_EMAIL = ['HR4U@ansrsource.com']
 
 MILESTONE_REPORTS_ADMIN_GROUP_NAME = "MilestoneReportsAdmin"
 
