15959d2d505f9aced01ad0ade52f1b6e934e9f79
janaki-jeevan <janaki.bs@ansrsource.com>
2017-10-06 10:20:50 +0530

    helper fixes

--- MyANSRSource/helper.py
+++ MyANSRSource/helper.py
@@ -14,27 +14,31 @@ def get_my_project_list(requestee, pmflag=0):
     delivery_manager = myansr_model.ProjectDetail.objects.filter(deliveryManager_id=requestee.id)
     pm = myansr_model.ProjectManager.objects.filter(user=requestee)
     acc_mgmt = company_model.Customer.objects.filter(Q(Crelation=requestee) | Q(Cdelivery=requestee))
-    if bu_head:
-        if requestee.is_superuser:
-            return myansr_model.Project.objects.all().values('id')
-        else:
-            return myansr_model.Project.objects.filter(bu__in=bu_head).values('id')
-
-    if pm_delegate and delivery_manager and portfolio_manager or not pm_delegate and delivery_manager and portfolio_manager:
-        if requestee.is_superuser:
-            return myansr_model.Project.objects.all().values('id')
-        else:
-            return myansr_model.ProjectDetail.objects.filter(
-                Q(deliveryManager_id=requestee.id) | Q(pmDelegate_id=requestee.id) | Q(
-                    portfolio_manager_id=requestee.id)).values('project_id')
-    if pm:
-        return myansr_model.ProjectManager.objects.filter(user=requestee).values('project__id')
 
     if requestee.is_superuser:
         return myansr_model.Project.objects.all().values('id')
+
+    elif bu_head:
+        return myansr_model.Project.objects.filter(bu__in=bu_head).values('id')
+
+    elif pm_delegate or delivery_manager or portfolio_manager or pm:
+        newlist = []
+        project_list = myansr_model.ProjectDetail.objects.filter(
+            Q(deliveryManager_id=requestee.id) | Q(pmDelegate_id=requestee.id) | Q(
+                portfolio_manager_id=requestee.id)).values('project_id')
+        for val in project_list:
+            newlist.append(val['project_id'])
+
+        return projectidlist(newlist, requestee)
+
     elif acc_mgmt:
         return myansr_model.Project.objects.filter(customer__in=acc_mgmt).values('id')
-    elif pm:
-        return myansr_model.ProjectManager.objects.filter(user=requestee).values('project__id')
     else:
         return []
+
+def projectidlist(newlist, requestee):
+    pm_list = myansr_model.ProjectManager.objects.filter(user=requestee).values('project__id')
+    for val in pm_list:
+        newlist.append(val['project__id'])
+        list(set(newlist))
+    return list(set(newlist))
