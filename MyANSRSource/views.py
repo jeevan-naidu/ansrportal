a024044aef99344df55bb9d966b156753c915c0f
janaki-jeevan <janaki.bs@ansrsource.com>
2017-10-31 12:41:49 +0530

    change requeats changes

--- MyANSRSource/views.py
+++ MyANSRSource/views.py
@@ -3284,9 +3284,15 @@ def saveProject(request):
                 pci.crId = u"BL-{0}".format(pr.id)
                 pci.reason = 'Base Line data'
                 pci.endDate = endDate
+                pci.startDate = startDate
                 pci.revisedEffort = int(request.POST.get('plannedEffort'))
                 pci.revisedTotal = float(request.POST.get('totalValue'))
                 pci.salesForceNumber = int(request.POST.get('salesForceNumber'))
+                pci.customerContact = str(request.POST.get('customerContact'))
+                if request.POST.get('signed') == 'True':
+                    pci.signed = '1'
+                elif request.POST.get('signed') == 'False':
+                    pci.signed = '0'
                 pci.save()
 
                 for eachId in eval(request.POST.get('pm')):
