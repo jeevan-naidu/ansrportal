5ba795d859270dd61335d610d48341bf2d757f3b
janaki-jeevan <janaki.bs@ansrsource.com>
2018-02-07 20:29:33 +0530

    changes

--- MyANSRSource/views.py
+++ MyANSRSource/views.py
@@ -2071,7 +2071,7 @@ class ApproveTimesheetView(TemplateView):
                 user_detail = []
                 for user in user_data:
                     if user.member.id != self.request.user.id:
-                        if (user.startDate <= start_date and user.endDate >= end_date) or (user.startDate in date_in_week) or (user.endDate in date_in_week) or (user.startDate <= start_date and user.endDate >= end_date):
+                        if (user.startDate <= start_date and user.endDate <= end_date) or (user.startDate in date_in_week) or (user.endDate in date_in_week) or (user.startDate <= start_date and user.endDate >= end_date):
                             ts_data = {}
                             ts_user_list = []
                             for data in data_for_week:
@@ -2110,7 +2110,7 @@ class ApproveTimesheetView(TemplateView):
                 ts_data_dict = {}
                 for user in user_data:
                     if user.active == True:
-                        if (user.startDate <= start_date and user.endDate >= end_date) or (user.startDate in date_in_week) or (user.endDate in date_in_week) or (user.startDate <= start_date and user.endDate >= end_date):
+                        if (user.startDate <= start_date and user.endDate <= end_date) or (user.startDate in date_in_week) or (user.endDate in date_in_week) or (user.startDate <= start_date and user.endDate >= end_date):
                             ts_data = {}
                             ts_user_list = []
                             if user.member_id != self.request.user.id:
