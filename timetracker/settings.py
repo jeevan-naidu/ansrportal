d281f1bec627c3ef0e1e51732dddca2b010fe58c
janaki-jeevan <janaki.bs@ansrsource.com>
2018-06-08 19:56:50 +0530

    changes

--- timetracker/settings.py
+++ timetracker/settings.py
@@ -223,7 +223,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "myansrsource",
         "USER": "root",
-        "PASSWORD": "hundred@100",
+        "PASSWORD": "root",
         "HOST": "localhost",
         "PORT": "3306",
         },
